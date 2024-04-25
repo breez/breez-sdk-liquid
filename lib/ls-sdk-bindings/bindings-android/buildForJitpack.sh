567b077717394c7065f758ed0ce0edf40a7af01d
Ross Savage <hello@satimoto.com>
2024-04-25 13:06:47 +0200

    Add building of android bindings and swift bindings for iOS

---
+++ lib/ls-sdk-bindings/bindings-android/buildForJitpack.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$JITPACK" ]]; then
+    exit 1
+fi
+
+echo "JitPack building version $VERSION."
+cd $(dirname $0)
+
+curl https://mvn.breez.technology/releases/ls_sdk/bindings-android/$VERSION/bindings-android-$VERSION.aar --output bindings-android-$VERSION.aar
+curl https://mvn.breez.technology/releases/ls_sdk/bindings-android/$VERSION/bindings-android-$VERSION.module --output bindings-android-$VERSION.module
+curl https://mvn.breez.technology/releases/ls_sdk/bindings-android/$VERSION/bindings-android-$VERSION-sources.jar --output bindings-android-$VERSION-sources.jar
+curl https://mvn.breez.technology/releases/ls_sdk/bindings-android/$VERSION/bindings-android-$VERSION.pom --output bindings-android-$VERSION.pom
+
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-$VERSION.aar -DpomFile=bindings-android-$VERSION.pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-$VERSION.module -DpomFile=bindings-android-$VERSION.pom
+mvn org.apache.maven.plugins:maven-install-plugin:3.1.1:install-file -Dfile=bindings-android-$VERSION-sources.jar -DpomFile=bindings-android-$VERSION.pom -Dclassifier=sources
+
+ls /home/jitpack/.m2/repository/ls_sdk/bindings-android
+echo "---"
+ls /home/jitpack/.m2/repository/ls_sdk/bindings-android/$VERSION/
---
+++ lib/ls-sdk-bindings/bindings-swift/ls_sdkFFI.xcframework/ios-arm64_x86_64-simulator/ls_sdkFFI.framework/Headers/ls_sdkFFI-umbrella.h
@@ -0,0 +1,1 @@
+#import "ls_sdkFFI.h"
