f26818252d14d512bfac7c6f258bc63821641065
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-04-29 13:26:49 +0300

    Create bindings for Dart/Flutter generation on Rust

---
+++ lib/ls-sdk-core/src/bindings.rs
@@ -0,0 +1,88 @@
+use anyhow::{anyhow, Result};
+use boltz_client::util::secrets::LBtcReverseRecovery;
+use std::sync::{Arc, OnceLock};
+
+use crate::{
+    model::{
+        Network, PaymentError, PrepareReceiveRequest, PrepareReceiveResponse, PrepareSendResponse,
+        ReceivePaymentResponse, SendPaymentResponse, WalletInfo,
+    },
+    wallet::Wallet,
+};
+
+use super::model::Payment;
+
+static WALLET_INSTANCE: OnceLock<Arc<Wallet>> = OnceLock::new();
+
+pub fn init(mnemonic: String, data_dir: Option<String>, network: Network) -> Result<()> {
+    let wallet = Wallet::init(&mnemonic, data_dir, network)?;
+    WALLET_INSTANCE.get_or_init(|| wallet);
+    Ok(())
+}
+
+pub fn get_info(with_scan: bool) -> Result<WalletInfo> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))?
+        .get_info(with_scan)
+}
+
+pub fn prepare_send_payment(invoice: String) -> Result<PrepareSendResponse, PaymentError> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .prepare_send_payment(&invoice)
+}
+
+pub fn send_payment(req: PrepareSendResponse) -> Result<SendPaymentResponse, PaymentError> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .send_payment(&req)
+}
+
+pub fn prepare_receive_payment(
+    req: PrepareReceiveRequest,
+) -> Result<PrepareReceiveResponse, PaymentError> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .prepare_receive_payment(&req)
+}
+
+pub fn receive_payment(
+    req: PrepareReceiveResponse,
+) -> Result<ReceivePaymentResponse, PaymentError> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .receive_payment(&req)
+}
+
+pub fn list_payments(with_scan: bool, include_pending: bool) -> Result<Vec<Payment>> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .list_payments(with_scan, include_pending)
+}
+
+pub fn recover_funds(recovery: LBtcReverseRecovery) -> Result<String> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .recover_funds(&recovery)
+}
+
+pub fn empty_wallet_cache() -> Result<()> {
+    WALLET_INSTANCE
+        .get()
+        .ok_or(anyhow!("Not initialized"))
+        .map_err(|e| PaymentError::Generic { err: e.to_string() })?
+        .empty_wallet_cache()
+}
--- lib/ls-sdk-core/src/lib.rs
+++ lib/ls-sdk-core/src/lib.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "frb")]
+pub mod bindings;
 pub mod error;
 pub mod model;
 pub mod persist;
