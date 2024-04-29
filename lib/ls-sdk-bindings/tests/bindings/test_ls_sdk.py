d71dd86b833983e3305374a3e9d8cf519eb98986
ok300 <106775972+ok300@users.noreply.github.com>
2024-04-29 11:54:55 +0200

    Add assertion in python binding test

--- lib/ls-sdk-bindings/tests/bindings/test_ls_sdk.py
+++ lib/ls-sdk-bindings/tests/bindings/test_ls_sdk.py
@@ -5,5 +5,6 @@ def test():
     sdk = ls_sdk.init(mnemonic, None, ls_sdk.Network.LIQUID_TESTNET)
     node_info = sdk.get_info(False)
     print(node_info)
+    assert node_info.pubkey == "03d902f35f560e0470c63313c7369168d9d7df2d49bf295fd9fb7cb109ccee0494"
 
 test()
