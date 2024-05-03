c10ddc7abf7ab465c10505c02cbcabbbec861273
ok300 <106775972+ok300@users.noreply.github.com>
2024-05-03 14:04:25 +0200

    Rename BindingWallet to BindingLiquidSdk

--- lib/bindings/src/lib.rs
+++ lib/bindings/src/lib.rs
@@ -16,54 +16,54 @@ impl From<anyhow::Error> for LsSdkError {
     }
 }
 
-pub fn connect(req: ConnectRequest) -> Result<Arc<BindingWallet>, LsSdkError> {
+pub fn connect(req: ConnectRequest) -> Result<Arc<BindingLiquidSdk>, LsSdkError> {
     let ln_sdk = LiquidSdk::connect(req)?;
-    Ok(Arc::from(BindingWallet { ln_sdk }))
+    Ok(Arc::from(BindingLiquidSdk { sdk: ln_sdk }))
 }
 
-pub struct BindingWallet {
-    ln_sdk: Arc<LiquidSdk>,
+pub struct BindingLiquidSdk {
+    sdk: Arc<LiquidSdk>,
 }
 
-impl BindingWallet {
+impl BindingLiquidSdk {
     pub fn get_info(&self, req: GetInfoRequest) -> Result<GetInfoResponse, LsSdkError> {
-        self.ln_sdk.get_info(req).map_err(Into::into)
+        self.sdk.get_info(req).map_err(Into::into)
     }
 
     pub fn prepare_send_payment(
         &self,
         req: PrepareSendRequest,
     ) -> Result<PrepareSendResponse, PaymentError> {
-        self.ln_sdk.prepare_send_payment(req)
+        self.sdk.prepare_send_payment(req)
     }
 
     pub fn send_payment(
         &self,
         req: PrepareSendResponse,
     ) -> Result<SendPaymentResponse, PaymentError> {
-        self.ln_sdk.send_payment(&req)
+        self.sdk.send_payment(&req)
     }
 
     pub fn prepare_receive_payment(
         &self,
         req: PrepareReceiveRequest,
     ) -> Result<PrepareReceiveResponse, PaymentError> {
-        self.ln_sdk.prepare_receive_payment(&req)
+        self.sdk.prepare_receive_payment(&req)
     }
 
     pub fn receive_payment(
         &self,
         req: PrepareReceiveResponse,
     ) -> Result<ReceivePaymentResponse, PaymentError> {
-        self.ln_sdk.receive_payment(&req)
+        self.sdk.receive_payment(&req)
     }
 
     pub fn backup(&self) -> Result<(), LsSdkError> {
-        self.ln_sdk.backup().map_err(Into::into)
+        self.sdk.backup().map_err(Into::into)
     }
 
     pub fn restore(&self, req: RestoreRequest) -> Result<(), LsSdkError> {
-        self.ln_sdk.restore(req).map_err(Into::into)
+        self.sdk.restore(req).map_err(Into::into)
     }
 }
 
