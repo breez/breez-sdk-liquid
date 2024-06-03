cbb217e4e79298fb665e03985f509dee3bf8e506
Ross Savage <hello@satimoto.com>
2024-06-03 09:01:02 +0200

    Add config to flutter example

---
+++ packages/flutter/example/lib/utils/config.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
+import 'package:path_provider/path_provider.dart';
+
+Future<Config> getConfig({
+  Network network = Network.mainnet,
+}) async {
+  debugPrint("Getting default SDK config for network: $network");
+  final defaultConf = defaultConfig(network: network);
+  debugPrint("Getting SDK config");
+  final workingDir = await getApplicationDocumentsDirectory();
+  return defaultConf.copyWith(
+    workingDir: workingDir.path,
+  );
+}
+
+extension ConfigCopyWith on Config {
+  Config copyWith({
+    String? boltzUrl,
+    String? electrumUrl,
+    String? workingDir,
+    Network? network,
+    BigInt? paymentTimeoutSec,
+  }) {
+    return Config(
+      boltzUrl: boltzUrl ?? this.boltzUrl,
+      electrumUrl: electrumUrl ?? this.electrumUrl,
+      workingDir: workingDir ?? this.workingDir,
+      network: network ?? this.network,
+      paymentTimeoutSec: paymentTimeoutSec ?? this.paymentTimeoutSec,
+    );
+  }
+}
