9161aa25be2eb1c1c051ea506baed4ad7711c857
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-03 20:55:02 +0300

    Reverse the payment list

--- packages/flutter/example/lib/routes/home/widgets/payment_list/payment_list.dart
+++ packages/flutter/example/lib/routes/home/widgets/payment_list/payment_list.dart
@@ -30,7 +30,7 @@ class PaymentList extends StatelessWidget {
           );
         }
 
-        final paymentList = paymentsSnapshot.data!;
+        List<Payment> paymentList = paymentsSnapshot.data!.reversed.toList();
 
         return RefreshIndicator(
           onRefresh: () async {
