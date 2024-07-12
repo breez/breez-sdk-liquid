daac27799b5ee937d0314710b96d9e2aff34da8e
Ross Savage <hello@satimoto.com>
2024-07-12 16:45:12 +0200

    Fix example Config

--- packages/flutter/example/lib/utils/config.dart
+++ packages/flutter/example/lib/utils/config.dart
@@ -18,6 +18,7 @@ extension ConfigCopyWith on Config {
   Config copyWith({
     String? liquidElectrumUrl,
     String? bitcoinElectrumUrl,
+    String? mempoolspaceUrl,
     String? workingDir,
     LiquidNetwork? network,
     BigInt? paymentTimeoutSec,
@@ -26,6 +27,7 @@ extension ConfigCopyWith on Config {
     return Config(
       liquidElectrumUrl: liquidElectrumUrl ?? this.liquidElectrumUrl,
       bitcoinElectrumUrl: bitcoinElectrumUrl ?? this.bitcoinElectrumUrl,
+      mempoolspaceUrl: mempoolspaceUrl ?? this.mempoolspaceUrl,
       workingDir: workingDir ?? this.workingDir,
       network: network ?? this.network,
       paymentTimeoutSec: paymentTimeoutSec ?? this.paymentTimeoutSec,
