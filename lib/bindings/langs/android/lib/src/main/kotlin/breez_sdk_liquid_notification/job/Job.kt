4e6da589fd0b7a5b465b2a47f9bd3a0b70ce7102
Ross Savage <hello@satimoto.com>
2024-08-06 14:57:05 +0200

    Add Android notification plugin

---
+++ lib/bindings/langs/android/lib/src/main/kotlin/breez_sdk_liquid_notification/job/Job.kt
@@ -0,0 +1,16 @@
+package breez_sdk_liquid_notification.job
+
+import breez_sdk_liquid.BindingLiquidSdk
+import breez_sdk_liquid.EventListener
+
+interface Job : EventListener {
+    /** When the notification service is connected to the Breez Liquid SDK
+     *  it calls `start` to initiate the job.
+     */
+    fun start(liquidSDK: BindingLiquidSdk)
+
+    /** When the short service timeout is reached it calls `onShutdown` 
+     *  to cleanup the job.
+     */
+    fun onShutdown()
+}
