111dea4c1dc4f3619de3cd010990509c8353e6c2
Ross Savage <hello@satimoto.com>
2024-08-15 12:22:15 +0200

    Fix swift plugin init

--- lib/bindings/langs/swift/Sources/BreezSDKLiquid/SDKNotificationService.swift
+++ lib/bindings/langs/swift/Sources/BreezSDKLiquid/SDKNotificationService.swift
@@ -8,10 +8,12 @@ open class SDKNotificationService: UNNotificationServiceExtension {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
     var currentTask: TaskProtocol?
-    var logger: ServiceLogger = ServiceLogger(logStream: nil)
+    public var logger: ServiceLogger = ServiceLogger(logStream: nil)
     
-    override init() { }
-    
+    public init(logger: Logger) {
+        self.logger = ServiceLogger(logStream: logger)
+    }
+
     override open func didReceive(
         _ request: UNNotificationRequest,
         withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void
@@ -88,7 +90,7 @@ open class SDKNotificationService: UNNotificationServiceExtension {
         self.currentTask?.onShutdown()
     }
     
-    func setLogger(logger: Logger) {
+    public func setLogger(logger: Logger) {
         self.logger = ServiceLogger(logStream: logger)
     }
 }
