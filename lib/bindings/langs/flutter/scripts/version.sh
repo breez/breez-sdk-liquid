0cb02ce43559214237b5e456b02e6114f29c63d7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-08-20 14:51:07 +0300

    Bugfix: Update production podpsec's version with version script (#447)
    * Add just recipe to update version number on podspec files & Cmake scripts
    
    * Resolve relative path of root from script directory
    
    * fix: Update production podpsec's version with version script
    
    * Run 'just version'
    
    * Update melos dependency
    
    * Check if versions on podspec & cmake scripts were updated properly on CI
    
    * Update versions on podspec & cmake scripts on publish
    
    * Declare license type on podspec
    
    * Add platform-specific link recipes
    
    * Add copy recipes to be used in CI workflows

--- lib/bindings/langs/flutter/scripts/version.sh
+++ lib/bindings/langs/flutter/scripts/version.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-ROOT="../../../../.."
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT="$SCRIPT_DIR/../../../../.."
 TAG_NAME=`awk '/^version: /{print $2}' $ROOT/packages/flutter/pubspec.yaml`
 
 # iOS & macOS
 APPLE_HEADER="version = '$TAG_NAME' # generated; do not edit"
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/ios/flutter_breez_liquid.podspec
+sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/ios/flutter_breez_liquid.podspec.production
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/macos/flutter_breez_liquid.podspec
 rm $ROOT/packages/flutter/macos/*.bak $ROOT/packages/flutter/ios/*.bak
 
