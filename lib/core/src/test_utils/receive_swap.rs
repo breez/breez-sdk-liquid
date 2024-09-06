b20c27a3998f75bb80f80c3c42352303d774e3df
yse <70684173+hydra-yse@users.noreply.github.com>
2024-09-06 21:58:12 +0200

    test: fix swap handlers tests by switching to mock chain service (#475)

--- lib/core/src/test_utils/receive_swap.rs
+++ lib/core/src/test_utils/receive_swap.rs
@@ -5,12 +5,9 @@ use std::sync::Arc;
 
 use tokio::sync::Mutex;
 
-use crate::{
-    chain::liquid::HybridLiquidChainService, model::Config, persist::Persister,
-    receive_swap::ReceiveSwapStateHandler,
-};
+use crate::{model::Config, persist::Persister, receive_swap::ReceiveSwapStateHandler};
 
-use super::{swapper::MockSwapper, wallet::MockWallet};
+use super::{chain::MockLiquidChainService, swapper::MockSwapper, wallet::MockWallet};
 
 pub(crate) fn new_receive_swap_state_handler(
     persister: Arc<Persister>,
@@ -18,7 +15,7 @@ pub(crate) fn new_receive_swap_state_handler(
     let config = Config::testnet();
     let onchain_wallet = Arc::new(MockWallet::new());
     let swapper = Arc::new(MockSwapper::new());
-    let liquid_chain_service = Arc::new(Mutex::new(HybridLiquidChainService::new(config.clone())?));
+    let liquid_chain_service = Arc::new(Mutex::new(MockLiquidChainService::new()));
 
     Ok(ReceiveSwapStateHandler::new(
         config,
--- lib/core/src/test_utils/send_swap.rs
+++ lib/core/src/test_utils/send_swap.rs
@@ -2,14 +2,11 @@
 
 use std::sync::Arc;
 
-use crate::{
-    chain::liquid::HybridLiquidChainService, model::Config, persist::Persister,
-    send_swap::SendSwapStateHandler,
-};
+use crate::{model::Config, persist::Persister, send_swap::SendSwapStateHandler};
 use anyhow::Result;
 use tokio::sync::Mutex;
 
-use super::{swapper::MockSwapper, wallet::MockWallet};
+use super::{chain::MockLiquidChainService, swapper::MockSwapper, wallet::MockWallet};
 
 pub(crate) fn new_send_swap_state_handler(
     persister: Arc<Persister>,
@@ -17,7 +14,7 @@ pub(crate) fn new_send_swap_state_handler(
     let config = Config::testnet();
     let onchain_wallet = Arc::new(MockWallet::new());
     let swapper = Arc::new(MockSwapper::new());
-    let chain_service = Arc::new(Mutex::new(HybridLiquidChainService::new(config.clone())?));
+    let chain_service = Arc::new(Mutex::new(MockLiquidChainService::new()));
 
     Ok(SendSwapStateHandler::new(
         config,
