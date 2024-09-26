e35b3eae67ea69c0bbecddb0e47bd408b07c15fd
Ross Savage <hello@satimoto.com>
2024-09-26 14:05:16 +0200

    Add logging

--- packages/dart/lib/breez_liquid.dart
+++ packages/dart/lib/breez_liquid.dart
@@ -9,6 +9,7 @@ export 'src/bindings/duplicates.dart';
 import 'dart:io';
 
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
+import 'package:logging/logging.dart';
 import 'src/frb_generated.dart';
 
 typedef BreezLiquid = RustLibApi;
@@ -16,6 +17,8 @@ typedef BreezLiquidImpl = RustLibApiImpl;
 
 const libName = 'libbreez_sdk_liquid_bindings.so';
 const iosLibName = "breez_sdk_liquidFFI";
+
+final _log = Logger('breez_liquid');
 
 class UnsupportedPlatform implements Exception {
   UnsupportedPlatform(String s);
@@ -28,13 +31,17 @@ Future<void> initialize({ExternalLibrary? dylib}) {
       dylib = ExternalLibrary.open(libName);
     } else if (Platform.isIOS || Platform.isMacOS) {
       try {
+        _log.info('Use iOS framework');
         dylib = ExternalLibrary.open("$iosLibName.framework/$iosLibName");
       } catch (e) {
+        _log.info('iOS framework not found, use ExternalLibrary.process');
         dylib = ExternalLibrary.process(iKnowHowToUseIt: true);
       }
     } else {
+      _log.info('${Platform.operatingSystem} is not yet supported!');
       throw UnsupportedPlatform('${Platform.operatingSystem} is not yet supported!');
     }
   }
+  _log.info('Return RustLib');
   return RustLib.init(externalLibrary: dylib);
 }
