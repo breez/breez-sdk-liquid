6d98dda2d85a0bcfd6494b21261f3542f26247e5
yse <hydra_yse@proton.me>
2024-09-28 15:54:06 +0200

    feat: add necessary fields to sync data

--- lib/core/src/sync/model/mod.rs
+++ lib/core/src/sync/model/mod.rs
@@ -1,15 +1,63 @@
+use boltz_client::boltz::SwapTree;
 use serde::{Deserialize, Serialize};
+
+use crate::prelude::Direction;
 
 pub(crate) mod sync;
 
 #[derive(Serialize, Deserialize)]
-pub(crate) enum ChainSyncData {}
+pub(crate) struct ClaimDetails {
+    pub(crate) swap_tree: SwapTree,
+    pub(crate) timeout_block_height: u32,
+    pub(crate) claim_fees_sat: u64,
+    pub(crate) claim_address: String,
+    pub(crate) claim_private_key: String,
+}
 
 #[derive(Serialize, Deserialize)]
-pub(crate) enum SendSyncData {}
+pub(crate) struct LockupDetails {
+    pub(crate) swap_tree: SwapTree,
+    pub(crate) timeout_block_height: u32,
+    pub(crate) lockup_address: String,
+    pub(crate) refund_private_key: String,
+}
 
 #[derive(Serialize, Deserialize)]
-pub(crate) enum ReceiveSyncData {}
+pub(crate) struct ChainSyncData {
+    pub(crate) swap_id: String,
+    pub(crate) preimage: String,
+    pub(crate) description: Option<String>,
+    pub(crate) direction: Direction,
+    pub(crate) claim_details: ClaimDetails,
+    pub(crate) lockup_details: LockupDetails,
+    pub(crate) payer_amount_sat: u64,
+    pub(crate) receiver_amount_sat: u64,
+    pub(crate) accept_zero_conf: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct SendSyncData {
+    pub(crate) swap_id: String,
+    pub(crate) preimage: Option<String>,
+    pub(crate) description: Option<String>,
+    pub(crate) refund_private_key: String,
+    pub(crate) swap_tree: SwapTree,
+    pub(crate) timeout_block_height: u32,
+    pub(crate) payer_amount_sat: u64,
+    pub(crate) receiver_amount_sat: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(crate) struct ReceiveSyncData {
+    pub(crate) swap_id: String,
+    pub(crate) preimage: String,
+    pub(crate) description: Option<String>,
+    pub(crate) claim_private_key: String,
+    pub(crate) swap_tree: SwapTree,
+    pub(crate) timeout_block_height: u32,
+    pub(crate) payer_amount_sat: u64,
+    pub(crate) receiver_amount_sat: u64,
+}
 
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "data_type", content = "data")]
