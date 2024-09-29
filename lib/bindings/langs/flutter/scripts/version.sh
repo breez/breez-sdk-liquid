97070f54826bb6d374cd2bb294692514c5cd98e5
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-09-29 18:42:44 +0300

    Remove CMake scripts

--- lib/bindings/langs/flutter/scripts/version.sh
+++ lib/bindings/langs/flutter/scripts/version.sh
@@ -12,14 +12,6 @@ sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/macos/flutter_breez_li
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/macos/flutter_breez_liquid.podspec.production
 rm $ROOT/packages/flutter/macos/*.bak $ROOT/packages/flutter/ios/*.bak
 
-# CMake platforms (Linux, Windows, and Android)
-CMAKE_HEADER="set(TagName \"v$TAG_NAME\") # generated; do not edit"
-for CMAKE_PLATFORM in android linux windows
-do
-    sed -i.bak "1 s/.*/$CMAKE_HEADER/" $ROOT/packages/flutter/$CMAKE_PLATFORM/CMakeLists.txt
-    rm $ROOT/packages/flutter/$CMAKE_PLATFORM/*.bak
-done
-
 GRADLE_HEADER="version '$TAG_NAME' \/\/ generated; do not edit"
 sed -i.bak "1 s/.*/$GRADLE_HEADER/" $ROOT/packages/flutter/android/build.gradle
 sed -i.bak "1 s/.*/$GRADLE_HEADER/" $ROOT/packages/flutter/android/build.gradle.production
