ad25d0e186645217c17da1976292e2f917df687c
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-09-29 18:43:07 +0300

    Remove breez_sdk_liquid.podspec from version script

--- lib/bindings/langs/flutter/scripts/version.sh
+++ lib/bindings/langs/flutter/scripts/version.sh
@@ -5,7 +5,6 @@ TAG_NAME=`awk '/^version: /{print $2}' $ROOT/packages/flutter/pubspec.yaml`
 
 # iOS & macOS
 APPLE_HEADER="version = '$TAG_NAME' # generated; do not edit"
-sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/ios/breez_sdk_liquid.podspec
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/ios/flutter_breez_liquid.podspec
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/ios/flutter_breez_liquid.podspec.production
 sed -i.bak "1 s/.*/$APPLE_HEADER/" $ROOT/packages/flutter/macos/flutter_breez_liquid.podspec
