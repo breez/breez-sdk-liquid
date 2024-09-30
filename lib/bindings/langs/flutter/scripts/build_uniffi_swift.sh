2941c3e51e164b45944a176d1427187db887143f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-09-30 16:04:54 +0300

    macOS: copy sources & framework file to macos folder on build-uniffi-swift script

--- lib/bindings/langs/flutter/scripts/build_uniffi_swift.sh
+++ lib/bindings/langs/flutter/scripts/build_uniffi_swift.sh
@@ -2,5 +2,8 @@
 cd ../..
 make init bindings-swift
 rm -rf ../../packages/flutter/ios/Frameworks/breez_sdk_liquidFFI.xcframework ../../packages/flutter/ios/Sources
+rm -rf ../../packages/flutter/ios/Frameworks/breez_sdk_liquidFFI.xcframework ../../packages/flutter/macos/Sources
 cp -r langs/swift/breez_sdk_liquidFFI.xcframework ../../packages/flutter/ios/Frameworks/breez_sdk_liquidFFI.xcframework
+cp -r langs/swift/breez_sdk_liquidFFI.xcframework ../../packages/flutter/macos/Frameworks/breez_sdk_liquidFFI.xcframework
 cp -r langs/swift/Sources ../../packages/flutter/ios/Sources
+cp -r langs/swift/Sources ../../packages/flutter/macos/Sources
