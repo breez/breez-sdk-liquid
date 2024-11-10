45a314347bfa84b6dd9adb593a1685af9e83ba7d
yse <hydra_yse@proton.me>
2024-11-10 03:16:39 +0100

    feat: add `SyncService`

--- lib/core/src/sync/mod.rs
+++ lib/core/src/sync/mod.rs
@@ -1,2 +1,33 @@
+use std::sync::Arc;
+
+use anyhow::{anyhow, Result};
+
+use crate::{persist::Persister, prelude::Signer};
+
+use self::client::SyncerClient;
 pub(crate) mod client;
 pub(crate) mod model;
+
+pub(crate) struct SyncService {
+    remote_url: String,
+    persister: Arc<Persister>,
+    signer: Arc<Box<dyn Signer>>,
+    client: Box<dyn SyncerClient>,
+}
+
+impl SyncService {
+    pub(crate) fn new(
+        remote_url: String,
+        persister: Arc<Persister>,
+        signer: Arc<Box<dyn Signer>>,
+        client: Box<dyn SyncerClient>,
+    ) -> Self {
+        Self {
+            remote_url,
+            persister,
+            signer,
+            client,
+        }
+    }
+
+}
