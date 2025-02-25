e09082578e6bbf7362559ac0e6bc449f6f99ee14
Ross Savage <hello@satimoto.com>
2025-02-25 10:49:04 +0100

    Define sdk types

--- lib/wasm/src/models.rs
+++ lib/wasm/src/models.rs
@@ -269,3 +269,482 @@ pub struct Symbol {
     pub rtl: Option<bool>,
     pub position: Option<u32>,
 }
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::Config)]
+pub struct Config {
+    pub liquid_electrum_url: String,
+    pub bitcoin_electrum_url: String,
+    pub mempoolspace_url: String,
+    pub working_dir: String,
+    pub cache_dir: Option<String>,
+    pub network: LiquidNetwork,
+    pub payment_timeout_sec: u64,
+    pub sync_service_url: Option<String>,
+    pub zero_conf_max_amount_sat: Option<u64>,
+    pub breez_api_key: Option<String>,
+    pub external_input_parsers: Option<Vec<ExternalInputParser>>,
+    pub use_default_external_input_parsers: bool,
+    pub onchain_fee_rate_leeway_sat_per_vbyte: Option<u32>,
+    pub asset_metadata: Option<Vec<AssetMetadata>>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::LiquidNetwork)]
+pub enum LiquidNetwork {
+    Mainnet,
+    Testnet,
+    Regtest,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ConnectRequest)]
+pub struct ConnectRequest {
+    pub config: Config,
+    pub mnemonic: Option<String>,
+    pub passphrase: Option<String>,
+    pub seed: Option<Vec<u8>>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PaymentMethod)]
+pub enum PaymentMethod {
+    Lightning,
+    BitcoinAddress,
+    LiquidAddress,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ReceiveAmount)]
+pub enum ReceiveAmount {
+    Bitcoin {
+        payer_amount_sat: u64,
+    },
+    Asset {
+        asset_id: String,
+        payer_amount: Option<f64>,
+    },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareReceiveRequest)]
+pub struct PrepareReceiveRequest {
+    pub payment_method: PaymentMethod,
+    pub amount: Option<ReceiveAmount>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareReceiveResponse)]
+pub struct PrepareReceiveResponse {
+    pub payment_method: PaymentMethod,
+    pub amount: Option<ReceiveAmount>,
+    pub fees_sat: u64,
+    pub min_payer_amount_sat: Option<u64>,
+    pub max_payer_amount_sat: Option<u64>,
+    pub swapper_feerate: Option<f64>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ReceivePaymentRequest)]
+pub struct ReceivePaymentRequest {
+    pub prepare_response: PrepareReceiveResponse,
+    pub description: Option<String>,
+    pub use_description_hash: Option<bool>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ReceivePaymentResponse)]
+pub struct ReceivePaymentResponse {
+    pub destination: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::Limits)]
+pub struct Limits {
+    pub min_sat: u64,
+    pub max_sat: u64,
+    pub max_zero_conf_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::LightningPaymentLimitsResponse)]
+pub struct LightningPaymentLimitsResponse {
+    pub send: Limits,
+    pub receive: Limits,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::OnchainPaymentLimitsResponse)]
+pub struct OnchainPaymentLimitsResponse {
+    pub send: Limits,
+    pub receive: Limits,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareSendRequest)]
+pub struct PrepareSendRequest {
+    pub destination: String,
+    pub amount: Option<PayAmount>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::SendDestination)]
+pub enum SendDestination {
+    LiquidAddress {
+        address_data: LiquidAddressData,
+    },
+    Bolt11 {
+        invoice: LNInvoice,
+        bip353_address: Option<String>,
+    },
+    Bolt12 {
+        offer: LNOffer,
+        receiver_amount_sat: u64,
+        bip353_address: Option<String>,
+    },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareSendResponse)]
+pub struct PrepareSendResponse {
+    pub destination: SendDestination,
+    pub fees_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::SendPaymentRequest)]
+pub struct SendPaymentRequest {
+    pub prepare_response: PrepareSendResponse,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::SendPaymentResponse)]
+pub struct SendPaymentResponse {
+    pub payment: Payment,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PayAmount)]
+pub enum PayAmount {
+    Bitcoin {
+        receiver_amount_sat: u64,
+    },
+    Asset {
+        asset_id: String,
+        receiver_amount: f64,
+    },
+    Drain,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PreparePayOnchainRequest)]
+pub struct PreparePayOnchainRequest {
+    pub amount: PayAmount,
+    pub fee_rate_sat_per_vbyte: Option<u32>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PreparePayOnchainResponse)]
+pub struct PreparePayOnchainResponse {
+    pub receiver_amount_sat: u64,
+    pub claim_fees_sat: u64,
+    pub total_fees_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PayOnchainRequest)]
+pub struct PayOnchainRequest {
+    pub address: String,
+    pub prepare_response: PreparePayOnchainResponse,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareRefundRequest)]
+pub struct PrepareRefundRequest {
+    pub swap_address: String,
+    pub refund_address: String,
+    pub fee_rate_sat_per_vbyte: u32,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareRefundResponse)]
+pub struct PrepareRefundResponse {
+    pub tx_vsize: u32,
+    pub tx_fee_sat: u64,
+    pub last_refund_tx_id: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::RefundRequest)]
+pub struct RefundRequest {
+    pub swap_address: String,
+    pub refund_address: String,
+    pub fee_rate_sat_per_vbyte: u32,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::RefundResponse)]
+pub struct RefundResponse {
+    pub refund_tx_id: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::AssetBalance)]
+pub struct AssetBalance {
+    pub asset_id: String,
+    pub balance_sat: u64,
+    pub name: Option<String>,
+    pub ticker: Option<String>,
+    pub balance: Option<f64>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::BlockchainInfo)]
+pub struct BlockchainInfo {
+    pub liquid_tip: u32,
+    pub bitcoin_tip: u32,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::WalletInfo)]
+pub struct WalletInfo {
+    pub balance_sat: u64,
+    pub pending_send_sat: u64,
+    pub pending_receive_sat: u64,
+    pub fingerprint: String,
+    pub pubkey: String,
+    pub asset_balances: Vec<AssetBalance>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::GetInfoResponse)]
+pub struct GetInfoResponse {
+    pub wallet_info: WalletInfo,
+    pub blockchain_info: BlockchainInfo,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::SignMessageRequest)]
+pub struct SignMessageRequest {
+    pub message: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::SignMessageResponse)]
+pub struct SignMessageResponse {
+    pub signature: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::CheckMessageRequest)]
+pub struct CheckMessageRequest {
+    pub message: String,
+    pub pubkey: String,
+    pub signature: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::CheckMessageResponse)]
+pub struct CheckMessageResponse {
+    pub is_valid: bool,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::BackupRequest)]
+pub struct BackupRequest {
+    pub backup_path: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::RestoreRequest)]
+pub struct RestoreRequest {
+    pub backup_path: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ListPaymentsRequest)]
+pub struct ListPaymentsRequest {
+    pub filters: Option<Vec<PaymentType>>,
+    pub states: Option<Vec<PaymentState>>,
+    pub from_timestamp: Option<i64>,
+    pub to_timestamp: Option<i64>,
+    pub offset: Option<u32>,
+    pub limit: Option<u32>,
+    pub details: Option<ListPaymentDetails>,
+    pub sort_ascending: Option<bool>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::ListPaymentDetails)]
+pub enum ListPaymentDetails {
+    Liquid {
+        asset_id: Option<String>,
+        destination: Option<String>,
+    },
+    Bitcoin {
+        address: Option<String>,
+    },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::GetPaymentRequest)]
+pub enum GetPaymentRequest {
+    PaymentHash { payment_hash: String },
+    SwapId { swap_id: String },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::RefundableSwap)]
+pub struct RefundableSwap {
+    pub swap_address: String,
+    pub timestamp: u32,
+    pub amount_sat: u64,
+    pub last_refund_tx_id: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PaymentState)]
+pub enum PaymentState {
+    Created = 0,
+    Pending = 1,
+    Complete = 2,
+    Failed = 3,
+    TimedOut = 4,
+    Refundable = 5,
+    RefundPending = 6,
+    WaitingFeeAcceptance = 7,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PaymentType)]
+pub enum PaymentType {
+    Receive = 0,
+    Send = 1,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PaymentStatus)]
+pub enum PaymentStatus {
+    Pending = 0,
+    Complete = 1,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::LnUrlInfo)]
+pub struct LnUrlInfo {
+    pub ln_address: Option<String>,
+    pub lnurl_pay_comment: Option<String>,
+    pub lnurl_pay_domain: Option<String>,
+    pub lnurl_pay_metadata: Option<String>,
+    pub lnurl_pay_success_action: Option<SuccessActionProcessed>,
+    pub lnurl_pay_unprocessed_success_action: Option<SuccessAction>,
+    pub lnurl_withdraw_endpoint: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::AssetMetadata)]
+pub struct AssetMetadata {
+    pub asset_id: String,
+    pub name: String,
+    pub ticker: String,
+    pub precision: u8,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::AssetInfo)]
+pub struct AssetInfo {
+    pub name: String,
+    pub ticker: String,
+    pub amount: f64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PaymentDetails)]
+pub enum PaymentDetails {
+    Lightning {
+        swap_id: String,
+        description: String,
+        liquid_expiration_blockheight: u32,
+        preimage: Option<String>,
+        invoice: Option<String>,
+        bolt12_offer: Option<String>,
+        payment_hash: Option<String>,
+        destination_pubkey: Option<String>,
+        lnurl_info: Option<LnUrlInfo>,
+        bip353_address: Option<String>,
+        claim_tx_id: Option<String>,
+        refund_tx_id: Option<String>,
+        refund_tx_amount_sat: Option<u64>,
+    },
+    Liquid {
+        destination: String,
+        description: String,
+        asset_id: String,
+        asset_info: Option<AssetInfo>,
+    },
+    Bitcoin {
+        swap_id: String,
+        description: String,
+        auto_accepted_fees: bool,
+        liquid_expiration_blockheight: Option<u32>,
+        bitcoin_expiration_blockheight: Option<u32>,
+        claim_tx_id: Option<String>,
+        refund_tx_id: Option<String>,
+        refund_tx_amount_sat: Option<u64>,
+    },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::Payment)]
+pub struct Payment {
+    pub destination: Option<String>,
+    pub tx_id: Option<String>,
+    pub unblinding_data: Option<String>,
+    pub timestamp: u32,
+    pub amount_sat: u64,
+    pub fees_sat: u64,
+    pub swapper_fees_sat: Option<u64>,
+    pub payment_type: PaymentType,
+    pub status: PaymentState,
+    pub details: PaymentDetails,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::RecommendedFees)]
+pub struct RecommendedFees {
+    pub fastest_fee: u64,
+    pub half_hour_fee: u64,
+    pub hour_fee: u64,
+    pub economy_fee: u64,
+    pub minimum_fee: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::BuyBitcoinProvider)]
+pub enum BuyBitcoinProvider {
+    Moonpay,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareBuyBitcoinRequest)]
+pub struct PrepareBuyBitcoinRequest {
+    pub provider: BuyBitcoinProvider,
+    pub amount_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareBuyBitcoinResponse)]
+pub struct PrepareBuyBitcoinResponse {
+    pub provider: BuyBitcoinProvider,
+    pub amount_sat: u64,
+    pub fees_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::BuyBitcoinRequest)]
+pub struct BuyBitcoinRequest {
+    pub prepare_response: PrepareBuyBitcoinResponse,
+    pub redirect_url: Option<String>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareLnUrlPayRequest)]
+pub struct PrepareLnUrlPayRequest {
+    pub data: LnUrlPayRequestData,
+    pub amount: PayAmount,
+    pub bip353_address: Option<String>,
+    pub comment: Option<String>,
+    pub validate_success_action_url: Option<bool>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::PrepareLnUrlPayResponse)]
+pub struct PrepareLnUrlPayResponse {
+    pub destination: SendDestination,
+    pub fees_sat: u64,
+    pub data: LnUrlPayRequestData,
+    pub comment: Option<String>,
+    pub success_action: Option<SuccessAction>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::model::LnUrlPayRequest)]
+pub struct LnUrlPayRequest {
+    pub prepare_response: PrepareLnUrlPayResponse,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::model::LnUrlPayResult)]
+pub enum LnUrlPayResult {
+    EndpointSuccess { data: LnUrlPaySuccessData },
+    EndpointError { data: LnUrlErrorData },
+    PayError { data: LnUrlPayErrorData },
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::model::LnUrlPaySuccessData)]
+pub struct LnUrlPaySuccessData {
+    pub payment: Payment,
+    pub success_action: Option<SuccessActionProcessed>,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::FetchPaymentProposedFeesRequest)]
+pub struct FetchPaymentProposedFeesRequest {
+    pub swap_id: String,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::FetchPaymentProposedFeesResponse)]
+pub struct FetchPaymentProposedFeesResponse {
+    pub swap_id: String,
+    pub fees_sat: u64,
+    pub payer_amount_sat: u64,
+    pub receiver_amount_sat: u64,
+}
+
+#[sdk_macros::extern_wasm_bindgen(breez_sdk_liquid::prelude::AcceptPaymentProposedFeesRequest)]
+pub struct AcceptPaymentProposedFeesRequest {
+    pub response: FetchPaymentProposedFeesResponse,
+}
