breez/breez-sdk-liquid

commit 9e83e06bd520d50f888f4a91dbe5feef0470a52a

Update selected built packages to Uniffi 0.28 (#766)

lib/bindings/langs/android/lib/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 33
         consumerProguardFiles("consumer-rules.pro")
     }
     

packages/flutter/android/build.gradle
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
     }
 }
 

packages/flutter/android/build.gradle.production
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
     }
 }
 

packages/react-native/android/build.gradle
@@ -26,7 +26,7 @@ apply plugin: 'kotlin-android'
 android {
     compileSdkVersion 34
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
         targetSdkVersion 34
         versionCode 1
         versionName "1.0"

packages/react-native/example/android/build.gradle
@@ -3,7 +3,7 @@
 buildscript {
     ext {
         buildToolsVersion = "33.0.0"
-        minSdkVersion = 24
+        minSdkVersion = 33
         compileSdkVersion = 34
         targetSdkVersion = 34
         kotlin_version = "1.8.0"

.github/workflows/build-bindings-android.yml
@@ -21,14 +21,35 @@ on:
         required: false
         type: boolean
         default: false
+      uniffi-25:
+        description: 'If true, builds additional bindings for Uniffi 0.25'
+        required: false
+        type: boolean
+        default: false
 
 jobs:
+  setup:
+    name: Setup
+    runs-on: ubuntu-latest
+    steps:
+      - id: set-matrix
+        run: |
+          if [ ${{ inputs.uniffi-25 }} == true ]; then
+            echo "::set-output name=matrix::['', '-uniffi-25']"
+          else
+            echo "::set-output name=matrix::['']"
+          fi
+    outputs:
+      uniffi-matrix: ${{ steps.set-matrix.outputs.matrix }}
+
   build:
     if: ${{ !inputs.use-dummy-binaries }}
     runs-on: ubuntu-latest
-    name: build ${{ matrix.target }}
+    name: build ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-linux-android,
           x86_64-linux-android,
@@ -55,12 +76,18 @@ jobs:
         
     - uses: Swatinem/rust-cache@v2
       with:
+        key: ${{ matrix.uniffi }}
         workspaces: lib
 
     - name: Build bindings
+      if: matrix.uniffi != '-uniffi-25'
       working-directory: lib/bindings
-      run: |
-        cargo ndk -t ${{ matrix.target }} build --release
+      run: cargo ndk -t ${{ matrix.target }} build --release
+
+    - name: Build bindings Uniffi 0.25
+      if: matrix.uniffi == '-uniffi-25'
+      working-directory: lib/bindings
+      run: cargo ndk -t ${{ matrix.target }} build --no-default-features --features=uniffi-25 --release
 
     - name: Copy build output
       run: |
@@ -73,36 +100,43 @@ jobs:
     - name: Archive release
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-${{ matrix.target }}
+        name: bindings-${{ matrix.target }}${{ matrix.uniffi }}
         path: dist/*
   
   jnilibs:
-    needs: build
+    needs: 
+    - setup
+    - build
     runs-on: ubuntu-latest
-    name: build jniLibs
+    name: build jniLibs${{ matrix.uniffi }}
+    strategy:
+      matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
     steps:
       - uses: actions/download-artifact@v4
         with:
-          name: bindings-aarch64-linux-android
+          name: bindings-aarch64-linux-android${{ matrix.uniffi }}
           path: arm64-v8a
 
       - uses: actions/download-artifact@v4
         with:
-          name: bindings-x86_64-linux-android
+          name: bindings-x86_64-linux-android${{ matrix.uniffi }}
           path: x86_64
       
       - name: Archive jniLibs
         uses: actions/upload-artifact@v4
         with:
-          name: bindings-android-jniLibs
+          name: bindings-android-jniLibs${{ matrix.uniffi }}
           path: ./*
 
   build-dummies:
     if: ${{ inputs.use-dummy-binaries }}
     runs-on: ubuntu-latest
-    name: build android dummies
+    name: build dummies ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-linux-android,
           x86_64-linux-android,
@@ -116,26 +150,31 @@ jobs:
       - name: Upload dummy Android ${{ matrix.target }} artifact
         uses: actions/upload-artifact@v4
         with:
-          name: bindings-${{ matrix.target }}
+          name: bindings-${{ matrix.target }}${{ matrix.uniffi }}
           path: ./*
 
   jnilibs-dummy:
-    needs: build-dummies
+    needs: 
+    - setup
+    - build-dummies
     runs-on: ubuntu-latest
-    name: build jniLibs dummy
+    name: build jniLibs dummy ${{ matrix.target }}${{ matrix.uniffi }}
+    strategy:
+      matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
     steps:
       - uses: actions/download-artifact@v4
         with:
-          name: bindings-aarch64-linux-android
+          name: bindings-aarch64-linux-android${{ matrix.uniffi }}
           path: arm64-v8a
 
       - uses: actions/download-artifact@v4
         with:
-          name: bindings-x86_64-linux-android
+          name: bindings-x86_64-linux-android${{ matrix.uniffi }}
           path: x86_64
       
       - name: Archive jniLibs
         uses: actions/upload-artifact@v4
         with:
-          name: bindings-android-jniLibs
+          name: bindings-android-jniLibs${{ matrix.uniffi }}
           path: ./*

.github/workflows/build-bindings-darwin.yml
@@ -21,14 +21,35 @@ on:
         required: false
         type: boolean
         default: false
+      uniffi-25:
+        description: 'If true, builds additional bindings for Uniffi 0.25'
+        required: false
+        type: boolean
+        default: false
 
 jobs:
+  setup:
+    name: Setup
+    runs-on: ubuntu-latest
+    steps:
+      - id: set-matrix
+        run: |
+          if [ ${{ inputs.uniffi-25 }} == true ]; then
+            echo "::set-output name=matrix::['', '-uniffi-25']"
+          else
+            echo "::set-output name=matrix::['']"
+          fi
+    outputs:
+      uniffi-matrix: ${{ steps.set-matrix.outputs.matrix }}
+
   build:
     if: ${{ !inputs.use-dummy-binaries }}
     runs-on: macOS-latest
-    name: build ${{ matrix.target }}
+    name: build ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-apple-darwin,
           x86_64-apple-darwin,
@@ -55,33 +76,45 @@ jobs:
         
     - uses: Swatinem/rust-cache@v2
       with:
+        key: ${{ matrix.uniffi }}
         workspaces: lib
 
     - name: Build bindings
+      if: matrix.uniffi != '-uniffi-25'
       working-directory: lib/bindings
       run: cargo lipo --release --targets ${{ matrix.target }}
+
+    - name: Build bindings Uniffi 0.25
+      if: matrix.uniffi == '-uniffi-25'
+      working-directory: lib/bindings
+      run: cargo lipo --no-default-features --features=uniffi-25 --release --targets ${{ matrix.target }}
     
     - name: Archive release
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-${{ matrix.target }}
+        name: bindings-${{ matrix.target }}${{ matrix.uniffi }}
         path: |
           lib/target/${{ matrix.target }}/release/libbreez_sdk_liquid_bindings.dylib
           lib/target/${{ matrix.target }}/release/libbreez_sdk_liquid_bindings.a
   
   merge:
     runs-on: macOS-latest
-    needs: build
-    name: build darwin-universal
+    needs: 
+    - setup
+    - build
+    name: build darwin-universal${{ matrix.uniffi }}
+    strategy:
+      matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
     steps:
     - uses: actions/download-artifact@v4
       with:
-        name: bindings-aarch64-apple-darwin
+        name: bindings-aarch64-apple-darwin${{ matrix.uniffi }}
         path: aarch64-apple-darwin
 
     - uses: actions/download-artifact@v4
       with:
-        name: bindings-x86_64-apple-darwin
+        name: bindings-x86_64-apple-darwin${{ matrix.uniffi }}
         path: x86_64-apple-darwin
 
     - name: Build Darwin universal
@@ -93,7 +126,7 @@ jobs:
     - name: Archive release
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-darwin-universal
+        name: bindings-darwin-universal${{ matrix.uniffi }}
         path: |
           darwin-universal/libbreez_sdk_liquid_bindings.dylib
           darwin-universal/libbreez_sdk_liquid_bindings.a
@@ -101,9 +134,12 @@ jobs:
   build-dummies:
     if: ${{ inputs.use-dummy-binaries }}
     runs-on: ubuntu-latest
-    name: build darwin dummies
+    name: build dummies ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: 
+    - setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-apple-darwin,
           x86_64-apple-darwin,
@@ -118,5 +154,5 @@ jobs:
       - name: Upload dummy darwin ${{ matrix.target }} artifact
         uses: actions/upload-artifact@v4
         with:
-          name: bindings-${{ matrix.target }}
+          name: bindings-${{ matrix.target }}${{ matrix.uniffi }}
           path: ./*

.github/workflows/build-bindings-ios.yml
@@ -21,14 +21,35 @@ on:
         required: false
         type: boolean
         default: false
+      uniffi-25:
+        description: 'If true, builds additional bindings for Uniffi 0.25'
+        required: false
+        type: boolean
+        default: false
 
 jobs:
+  setup:
+    name: Setup
+    runs-on: ubuntu-latest
+    steps:
+      - id: set-matrix
+        run: |
+          if [ ${{ inputs.uniffi-25 }} == true ]; then
+            echo "::set-output name=matrix::['', '-uniffi-25']"
+          else
+            echo "::set-output name=matrix::['']"
+          fi
+    outputs:
+      uniffi-matrix: ${{ steps.set-matrix.outputs.matrix }}
+
   build:
     if: ${{ !inputs.use-dummy-binaries }}
     runs-on: macOS-latest
-    name: build ${{ matrix.target }}
+    name: build ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-apple-ios,
           x86_64-apple-ios,
@@ -58,6 +79,7 @@ jobs:
         
     - uses: Swatinem/rust-cache@v2
       with:
+        key: ${{ matrix.uniffi }}
         workspaces: lib
 
     - name: Install xcode
@@ -66,33 +88,44 @@ jobs:
         xcode-version: latest-stable
 
     - name: Build bindings
+      if: matrix.uniffi != '-uniffi-25'
       working-directory: lib/bindings
       run: cargo build --release --target ${{ matrix.target }}
+
+    - name: Build bindings Uniffi 0.25
+      if: matrix.uniffi == '-uniffi-25'
+      working-directory: lib/bindings
+      run: cargo build --no-default-features --features=uniffi-25 --release --target ${{ matrix.target }}
     
     - name: Archive release
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-${{ matrix.target }}
+        name: bindings-${{ matrix.target }}${{ matrix.uniffi }}
         path: lib/target/${{ matrix.target }}/release/libbreez_sdk_liquid_bindings.a
 
   merge:
     runs-on: macOS-latest
-    needs: build
-    name: build ios-universal
+    needs: 
+    - setup
+    - build
+    name: build ios-universal${{ matrix.uniffi }}
+    strategy:
+      matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
     steps:
     - uses: actions/download-artifact@v4
       with:
-        name: bindings-aarch64-apple-ios
+        name: bindings-aarch64-apple-ios${{ matrix.uniffi }}
         path: aarch64-apple-ios
 
     - uses: actions/download-artifact@v4
       with:
-        name: bindings-x86_64-apple-ios
+        name: bindings-x86_64-apple-ios${{ matrix.uniffi }}
         path: x86_64-apple-ios
 
     - uses: actions/download-artifact@v4
       with:
-        name: bindings-aarch64-apple-ios-sim
+        name: bindings-aarch64-apple-ios-sim${{ matrix.uniffi }}
         path: aarch64-apple-ios-sim
 
     - name: Build ios-universal
@@ -108,21 +141,23 @@ jobs:
     - name: Archive ios-universal
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-ios-universal
+        name: bindings-ios-universal${{ matrix.uniffi }}
         path: ios-universal/libbreez_sdk_liquid_bindings.a
 
     - name: Archive ios-universal-sim
       uses: actions/upload-artifact@v4
       with:
-        name: bindings-ios-universal-sim
+        name: bindings-ios-universal-sim${{ matrix.uniffi }}
         path: ios-universal-sim/libbreez_sdk_liquid_bindings.a
 
   build-dummies:
     if: ${{ inputs.use-dummy-binaries }}
     runs-on: ubuntu-latest
-    name: build ios dummies
+    name: build dummies ${{ matrix.target }}${{ matrix.uniffi }}
+    needs: setup
     strategy:
       matrix:
+        uniffi: ${{ fromJson(needs.setup.outputs.uniffi-matrix) }}
         target: [
           aarch64-apple-ios,
           x86_64-apple-ios,
@@ -138,5 +173,5 @@ jobs:
       - name: Upload dummy ios ${{ matrix.target }} artifact
         uses: actions/upload-artifact@v4
         with:
-          name: bindings-${{ matrix.target }}
+          name: bindings-${{ matrix.target }}{{ matrix.uniffi }}
           path: ./*

commit 277486f1aea7b4ad824635501be08dcd517affc3

fix: revert minSdkVersion across all uniffi packages
This PR fixes a regression introduced in https://github.com/breez/breez-sdk-liquid/pull/766 due to a confusion between minSdkVersion requirement & minimum compileSdkVersion requirement.

lib/bindings/langs/android/lib/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 33
+        minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
     }
     

packages/flutter/android/build.gradle
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
     }
 }
 

packages/flutter/android/build.gradle.production
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
     }
 }
 

packages/react-native/android/build.gradle
@@ -26,7 +26,7 @@ apply plugin: 'kotlin-android'
 android {
     compileSdkVersion 34
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
         targetSdkVersion 34
         versionCode 1
         versionName "1.0"

packages/react-native/example/android/build.gradle
@@ -3,7 +3,7 @@
 buildscript {
     ext {
         buildToolsVersion = "33.0.0"
-        minSdkVersion = 33
+        minSdkVersion = 24
         compileSdkVersion = 34
         targetSdkVersion = 34
         kotlin_version = "1.8.0"