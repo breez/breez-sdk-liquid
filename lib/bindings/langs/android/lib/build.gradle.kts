breez/breez-sdk-liquid

commit 9e83e06bd520d50f888f4a91dbe5feef0470a52a

Update selected built packages to Uniffi 0.28 (#766)

.github/workflows/build-language-bindings.yml
@@ -60,6 +60,7 @@ on:
 jobs:
   build-language-bindings:
     runs-on: ubuntu-latest
+    if: ${{ inputs.kotlin || inputs.swift || inputs.python }}
     steps:
       - name: Checkout breez-sdk-liquid repo
         uses: actions/checkout@v4
@@ -77,12 +78,16 @@ jobs:
         with:
           version: "27.2"
           repo-token: ${{ secrets.GITHUB_TOKEN }}
+        
+      - uses: Swatinem/rust-cache@v2
+        with:
+          workspaces: lib
 
       - name: Build Kotlin binding
         if: ${{ inputs.kotlin }}
         working-directory: lib/bindings
         run: |
-          cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language kotlin -o ffi/kotlin
+          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language kotlin -o ffi/kotlin
   
       - name: Archive Kotlin binding
         if: ${{ inputs.kotlin }}
@@ -90,19 +95,12 @@ jobs:
         with:
           name: bindings-kotlin
           path: lib/bindings/ffi/kotlin/breez_sdk_liquid/breez_sdk_liquid.kt
-
-      - name: Archive Kotlin multiplatform binding
-        if: ${{ inputs.kotlin }}
-        uses: actions/upload-artifact@v4
-        with:
-          name: bindings-kotlin-multiplatform
-          path: lib/bindings/ffi/kmp/*
       
       - name: Build Swift binding
         if: ${{ inputs.swift }}
         working-directory: lib/bindings
         run: |
-          cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift --out-dir langs/swift/Sources/BreezSDKLiquid
+          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift --out-dir langs/swift/Sources/BreezSDKLiquid
           mv langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquid.swift langs/swift/Sources/BreezSDKLiquid/BreezSDKLiquid.swift
       
       - name: Archive Swift binding
@@ -116,7 +114,7 @@ jobs:
         if: ${{ inputs.python }}
         working-directory: lib/bindings
         run: |
-            cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language python -o ffi/python
+            cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language python -o ffi/python
     
       - name: Archive Python binding
         if: ${{ inputs.python }}
@@ -124,6 +122,45 @@ jobs:
         with:
           name: bindings-python
           path: lib/bindings/ffi/python/breez_sdk_liquid.py
+
+  build-language-bindings-uniffi-25:
+    runs-on: ubuntu-latest
+    if: ${{ inputs.kotlin || inputs.csharp || inputs.golang }}
+    steps:
+      - name: Checkout breez-sdk-liquid repo
+        uses: actions/checkout@v4
+        with:
+          repository: ${{ inputs.repository || github.repository }}
+          ref: ${{ inputs.ref || github.sha }}
+
+      - name: Install rust
+        run: |
+          rustup set auto-self-update disable
+          rustup toolchain install stable --profile minimal
+
+      - name: Install Protoc
+        uses: arduino/setup-protoc@v3
+        with:
+          version: "27.2"
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+
+      - uses: Swatinem/rust-cache@v2
+        with:
+          key: uniffi-25
+          workspaces: lib
+
+      - name: Build Kotlin binding
+        if: ${{ inputs.kotlin }}
+        working-directory: lib/bindings
+        run: |
+          cargo run --no-default-features --features=uniffi-25 --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language kotlin -o ffi/kotlin
+
+      - name: Archive Kotlin multiplatform binding
+        if: ${{ inputs.kotlin }}
+        uses: actions/upload-artifact@v4
+        with:
+          name: bindings-kotlin-multiplatform
+          path: lib/bindings/ffi/kmp/*
 
       - name: Build C# binding
         if: ${{ inputs.csharp }}

.github/workflows/main.yml
@@ -153,15 +153,55 @@ jobs:
           version: "27.2"
           repo-token: ${{ secrets.GITHUB_TOKEN }}
 
+      - name: Setup python
+        uses: actions/setup-python@v4
+        with:
+          python-version: '3.11'
+      
+      - name: Build bindings
+        working-directory: lib/bindings
+        run: cargo build
+
+      - name: Run bindings tests
+        run: |
+          curl -o jna-5.12.1.jar https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar
+          export CLASSPATH=$(pwd)/jna-5.12.1.jar;
+          cd lib/bindings
+          cargo test
+
+  build-bindings-uniffi-25:
+    name: Test bindings Uniffi 0.25
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repo
+        uses: actions/checkout@v4
+
+      - uses: Swatinem/rust-cache@v2
+        with:
+          key: -uniffi-25
+          workspaces: |
+            lib -> target
+            cli -> target
+
+      - name: Install Protoc
+        uses: arduino/setup-protoc@v3
+        with:
+          version: "27.2"
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+
       - name: Setup dotnet
-        if: ${{ !inputs.skip-tests }}
         uses: actions/setup-dotnet@v3
         with:
           dotnet-version: '7.0.x'
 
+      - name: Setup go
+        uses: actions/setup-go@v4
+        with:
+          go-version: '1.19.9'
+
       - name: Build bindings
         working-directory: lib/bindings
-        run: cargo build
+        run: cargo build --no-default-features --features uniffi-25
 
       - name: Build C# bindings
         working-directory: lib/bindings       
@@ -178,22 +218,9 @@ jobs:
           cp ../target/debug/libbreez_sdk_liquid_bindings.so ffi/golang
           cp -r ffi/golang/breez_sdk_liquid tests/bindings/golang/
 
-      - name: Setup go
-        uses: actions/setup-go@v4
-        with:
-          go-version: '1.19.9'
-
-      - name: Setup python
-        uses: actions/setup-python@v4
-        with:
-          python-version: '3.11'
-
       - name: Run bindings tests
-        run: |
-          curl -o jna-5.12.1.jar https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar
-          export CLASSPATH=$(pwd)/jna-5.12.1.jar;
-          cd lib/bindings
-          cargo test
+        working-directory: lib/bindings       
+        run: cargo test --no-default-features --features uniffi-25
 
   build-wasm:
     name: Test WASM
@@ -255,7 +282,7 @@ jobs:
       - name: Build Android bindings
         working-directory: lib/bindings       
         run: |
-          cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o langs/android/lib/src/main/kotlin
+          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o langs/android/lib/src/main/kotlin
 
       - name: Run Android build
         working-directory: lib/bindings/langs/android  
@@ -265,7 +292,7 @@ jobs:
       - name: Build Swift bindings
         working-directory: lib/bindings       
         run: |
-          cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift -o langs/swift/Sources/BreezSDKLiquid
+          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift -o langs/swift/Sources/BreezSDKLiquid
           mv langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquid.swift langs/swift/Sources/BreezSDKLiquid/BreezSDKLiquid.swift
           cp langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.h langs/swift/breez_sdk_liquidFFI.xcframework/ios-arm64/breez_sdk_liquidFFI.framework/Headers
           cp langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.h langs/swift/breez_sdk_liquidFFI.xcframework/ios-arm64_x86_64-simulator/breez_sdk_liquidFFI.framework/Headers

.github/workflows/publish-all-platforms.yml
@@ -121,6 +121,7 @@ jobs:
       swift-package-version: ${{ needs.pre-setup.outputs.swift-package-version || '0.0.2' }}
       publish: ${{ needs.pre-setup.outputs.publish }}
       use-dummy-binaries: ${{ needs.pre-setup.outputs.use-dummy-binaries }}
+      uniffi-25: ${{ !!needs.pre-setup.outputs.csharp-package-version || !!needs.pre-setup.outputs.golang-package-version || !!needs.pre-setup.outputs.kotlin-multiplatform-package-version }}
     steps:
       - run: echo "set setup output variables"
 
@@ -132,6 +133,7 @@ jobs:
       repository: ${{ needs.setup.outputs.repository }}
       ref: ${{ needs.setup.outputs.ref }}
       use-dummy-binaries: ${{ needs.setup.outputs.use-dummy-binaries == 'true' }}
+      uniffi-25: ${{ needs.setup.outputs.uniffi-25 == 'true' }}
 
   build-bindings-darwin:
     needs: setup
@@ -141,6 +143,7 @@ jobs:
       repository: ${{ needs.setup.outputs.repository }}
       ref: ${{ needs.setup.outputs.ref }}
       use-dummy-binaries: ${{ needs.setup.outputs.use-dummy-binaries == 'true' }}
+      uniffi-25: ${{ needs.setup.outputs.uniffi-25 == 'true' }}
 
   build-bindings-linux:
     needs: setup
@@ -150,6 +153,7 @@ jobs:
       repository: ${{ needs.setup.outputs.repository }}
       ref: ${{ needs.setup.outputs.ref }}
       use-dummy-binaries: ${{ needs.setup.outputs.use-dummy-binaries == 'true' }}
+      uniffi-25: ${{ needs.setup.outputs.uniffi-25 == 'true' }}
 
   build-bindings-android:
     needs: setup
@@ -159,6 +163,7 @@ jobs:
       repository: ${{ needs.setup.outputs.repository }}
       ref: ${{ needs.setup.outputs.ref }}
       use-dummy-binaries: ${{ needs.setup.outputs.use-dummy-binaries == 'true' }}
+      uniffi-25: ${{ needs.setup.outputs.uniffi-25 == 'true' }}
 
   build-bindings-ios:
     needs: setup
@@ -168,6 +173,7 @@ jobs:
       repository: ${{ needs.setup.outputs.repository }}
       ref: ${{ needs.setup.outputs.ref }}
       use-dummy-binaries: ${{ needs.setup.outputs.use-dummy-binaries == 'true' }}
+      uniffi-25: ${{ needs.setup.outputs.uniffi-25 == 'true' }}
 
   build-language-bindings:
     needs: setup

lib/bindings/langs/android/lib/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 33
         consumerProguardFiles("consumer-rules.pro")
     }
     

lib/bindings/makefile
@@ -37,14 +37,25 @@ all: bindings-swift bindings-android python-darwin react-native
 ## Android 
 .PHONY: android
 android: aarch64-linux-android x86_64-linux-android
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o ffi/kotlin
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o ffi/kotlin
+
+android-uniffi-25: aarch64-linux-android-uniffi-25 x86_64-linux-android-uniffi-25
+	cargo run --no-default-features --features=uniffi-25 --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o ffi/kotlin
 
 aarch64-linux-android: $(SOURCES) ndk-home
 	cargo ndk -t aarch64-linux-android -o ffi/kotlin/jniLibs build --release	
 	cp -a $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(OS_NAME)-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so ffi/kotlin/jniLibs/arm64-v8a/
 
+aarch64-linux-android-uniffi-25: $(SOURCES) ndk-home
+	cargo ndk -t aarch64-linux-android -o ffi/kotlin/jniLibs build --no-default-features --features=uniffi-25 --release	
+	cp -a $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(OS_NAME)-x86_64/sysroot/usr/lib/aarch64-linux-android/libc++_shared.so ffi/kotlin/jniLibs/arm64-v8a/
+
 x86_64-linux-android: $(SOURCES) ndk-home
 	cargo ndk -t x86_64-linux-android -o ffi/kotlin/jniLibs build --release
+	cp -a $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(OS_NAME)-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so ffi/kotlin/jniLibs/x86_64/
+
+x86_64-linux-android-uniffi-25: $(SOURCES) ndk-home
+	cargo ndk -t x86_64-linux-android -o ffi/kotlin/jniLibs build --no-default-features --features=uniffi-25 --release
 	cp -a $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(OS_NAME)-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so ffi/kotlin/jniLibs/x86_64/
 
 bindings-android: android
@@ -58,9 +69,9 @@ bindings-android: android
 .PHONY: kotlin
 kotlin: $(SOURCES)
 	cargo build --release --target $(TARGET)
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o ffi/kotlin
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o ffi/kotlin
 
-bindings-kotlin-multiplatform: ios-universal android
+bindings-kotlin-multiplatform: ios-universal-uniffi-25 android-uniffi-25
 	mkdir -p langs/kotlin-multiplatform/breez-sdk-liquid-kmp/src/androidMain
 	cp -r ffi/kotlin/jniLibs/ langs/kotlin-multiplatform/breez-sdk-liquid-kmp/src/androidMain/jniLibs/
 	cp -r ffi/kmp/* langs/kotlin-multiplatform/breez-sdk-liquid-kmp/src/
@@ -88,6 +99,17 @@ ios-universal: $(SOURCES)
 	# build universal lib for arm sim and x86 sim
 	lipo -create -output ../target/ios-universal-sim/release/libbreez_sdk_liquid_bindings.a ../target/aarch64-apple-ios-sim/release/libbreez_sdk_liquid_bindings.a ../target/x86_64-apple-ios/release/libbreez_sdk_liquid_bindings.a
 
+ios-universal-uniffi-25: $(SOURCES)		
+	mkdir -p ../target/ios-universal/release
+	mkdir -p ../target/ios-universal-sim/release
+	cargo build --no-default-features --features uniffi-25 --release --target aarch64-apple-ios ;\
+	cargo build --no-default-features --features uniffi-25 --release --target x86_64-apple-ios ;\
+	cargo build --no-default-features --features uniffi-25 --release --target aarch64-apple-ios-sim ;\
+	# build universal lib for arm device and x86 sim
+	lipo -create -output ../target/ios-universal/release/libbreez_sdk_liquid_bindings.a ../target/aarch64-apple-ios/release/libbreez_sdk_liquid_bindings.a ../target/x86_64-apple-ios/release/libbreez_sdk_liquid_bindings.a
+	# build universal lib for arm sim and x86 sim
+	lipo -create -output ../target/ios-universal-sim/release/libbreez_sdk_liquid_bindings.a ../target/aarch64-apple-ios-sim/release/libbreez_sdk_liquid_bindings.a ../target/x86_64-apple-ios/release/libbreez_sdk_liquid_bindings.a
+
 darwin-universal: $(SOURCES)
 	mkdir -p ../target/darwin-universal/release
 	cargo lipo --release --targets aarch64-apple-darwin
@@ -95,14 +117,21 @@ darwin-universal: $(SOURCES)
 	lipo -create -output ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ../target/aarch64-apple-darwin/release/libbreez_sdk_liquid_bindings.dylib ../target/x86_64-apple-darwin/release/libbreez_sdk_liquid_bindings.dylib
 	lipo -create -output ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.a ../target/aarch64-apple-darwin/release/libbreez_sdk_liquid_bindings.a ../target/x86_64-apple-darwin/release/libbreez_sdk_liquid_bindings.a
 
+darwin-universal-uniffi-25: $(SOURCES)
+	mkdir -p ../target/darwin-universal/release
+	cargo lipo --no-default-features --features uniffi-25 --release --targets aarch64-apple-darwin
+	cargo lipo --no-default-features --features uniffi-25 --release --targets x86_64-apple-darwin
+	lipo -create -output ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ../target/aarch64-apple-darwin/release/libbreez_sdk_liquid_bindings.dylib ../target/x86_64-apple-darwin/release/libbreez_sdk_liquid_bindings.dylib
+	lipo -create -output ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.a ../target/aarch64-apple-darwin/release/libbreez_sdk_liquid_bindings.a ../target/x86_64-apple-darwin/release/libbreez_sdk_liquid_bindings.a
+
 ## Swift
 swift-ios: ios-universal
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl -l swift -o ffi/swift-ios
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl -l swift -o ffi/swift-ios
 	cp ../target/ios-universal/release/libbreez_sdk_liquid_bindings.a ffi/swift-ios
 	cd ffi/swift-ios && "swiftc" "-emit-module" "-module-name" "breez_sdk_liquid_bindings" "-Xcc" "-fmodule-map-file=$(CURRENT_DIR)/ffi/swift-ios/breez_sdk_liquidFFI.modulemap" "-I" "."  "-L" "." "-lbreez_sdk_liquid_bindings" breez_sdk_liquid.swift
 
 swift-darwin: darwin-universal
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl -l swift -o ffi/swift-darwin
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl -l swift -o ffi/swift-darwin
 	cp ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ffi/swift-darwin
 	cd ffi/swift-darwin && "swiftc" "-emit-module" "-module-name" "breez_sdk_liquid_bindings" "-Xcc" "-fmodule-map-file=$(CURRENT_DIR)/ffi/swift-darwin/breez_sdk_liquidFFI.modulemap" "-I" "."  "-L" "." "-lbreez_sdk_liquid_bindings" breez_sdk_liquid.swift
 
@@ -110,7 +139,7 @@ bindings-swift: ios-universal darwin-universal build-ios-framework
 
 build-ios-framework:
 	mkdir -p langs/swift/Sources/BreezSDKLiquid
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift -o langs/swift/Sources/BreezSDKLiquid
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language swift -o langs/swift/Sources/BreezSDKLiquid
 	mv langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquid.swift langs/swift/Sources/BreezSDKLiquid/BreezSDKLiquid.swift
 	cp langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.h langs/swift/breez_sdk_liquidFFI.xcframework/ios-arm64/breez_sdk_liquidFFI.framework/Headers
 	cp langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.h langs/swift/breez_sdk_liquidFFI.xcframework/ios-arm64_x86_64-simulator/breez_sdk_liquidFFI.framework/Headers
@@ -122,19 +151,19 @@ build-ios-framework:
 	rm langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.h
 	rm langs/swift/Sources/BreezSDKLiquid/breez_sdk_liquidFFI.modulemap
 
-csharp-darwin: darwin-universal
+csharp-darwin: darwin-universal-uniffi-25
 	cargo install uniffi-bindgen-cs --git https://github.com/NordSecurity/uniffi-bindgen-cs --tag v0.8.0+v0.25.0
 	uniffi-bindgen-cs src/breez_sdk_liquid.udl -o ffi/csharp -c ./uniffi.toml
 	cp ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ffi/csharp
 
 csharp-linux: $(SOURCES)
 	cargo install uniffi-bindgen-cs --git https://github.com/NordSecurity/uniffi-bindgen-cs --tag v0.8.0+v0.25.0
-	cargo build --release --target $(TARGET)
+	cargo build --no-default-features --features uniffi-25 --release --target $(TARGET)
 	uniffi-bindgen-cs src/breez_sdk_liquid.udl -o ffi/csharp -c ./uniffi.toml
 	cp ../target/$(TARGET)/release/libbreez_sdk_liquid_bindings.so ffi/csharp
 
 ## Go
-golang-darwin: darwin-universal
+golang-darwin: darwin-universal-uniffi-25
 	cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go --tag v0.2.1+v0.25.0
 	uniffi-bindgen-go src/breez_sdk_liquid.udl -o ffi/golang -c ./uniffi.toml
 	cp ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ffi/golang
@@ -142,7 +171,7 @@ golang-darwin: darwin-universal
 
 golang-linux: $(SOURCES)
 	cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go --tag v0.2.1+v0.25.0
-	cargo build --release --target $(TARGET)
+	cargo build --no-default-features --features uniffi-25 --release --target $(TARGET)
 	uniffi-bindgen-go src/breez_sdk_liquid.udl -o ffi/golang -c ./uniffi.toml
 	cp ../target/$(TARGET)/release/libbreez_sdk_liquid_bindings.so ffi/golang
 	cp -r ffi/golang/breez_sdk_liquid tests/bindings/golang/
@@ -150,11 +179,11 @@ golang-linux: $(SOURCES)
 ## Python
 python-linux: $(SOURCES)
 	cargo build --release --target $(TARGET)
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language python -o ffi/python
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language python -o ffi/python
 	cp ../target/$(TARGET)/release/libbreez_sdk_liquid_bindings.so ffi/python
 
 python-darwin: darwin-universal
-	cargo run --features=uniffi/cli --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language python -o ffi/python
+	cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language python -o ffi/python
 	cp ../target/darwin-universal/release/libbreez_sdk_liquid_bindings.dylib ffi/python
 
 ## React Native

packages/flutter/android/build.gradle
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
     }
 }
 

packages/flutter/android/build.gradle.production
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
     }
 }
 

packages/react-native/android/build.gradle
@@ -26,7 +26,7 @@ apply plugin: 'kotlin-android'
 android {
     compileSdkVersion 34
     defaultConfig {
-        minSdkVersion 24
+        minSdkVersion 33
         targetSdkVersion 34
         versionCode 1
         versionName "1.0"

commit 573a34580135125cc80cb345a10f13307bfad8a6

Downgrade android release to Uniffi 0.25 (#786)
* fix: revert minSdkVersion across all uniffi packages

This PR fixes a regression introduced in https://github.com/breez/breez-sdk-liquid/pull/766 due to a confusion between minSdkVersion requirement & minimum compileSdkVersion requirement.

* Bump package requirements on flutter_breez_liquid README

* Bump JNA used on bindings tests to 5.14.0

* Downgrade android release to Uniffi 0.25

---------

Co-authored-by: Ross Savage <hello@satimoto.com>

.github/workflows/build-language-bindings.yml
@@ -60,7 +60,7 @@ on:
 jobs:
   build-language-bindings:
     runs-on: ubuntu-latest
-    if: ${{ inputs.kotlin || inputs.swift || inputs.python }}
+    if: ${{ inputs.swift || inputs.python }}
     steps:
       - name: Checkout breez-sdk-liquid repo
         uses: actions/checkout@v4
@@ -82,19 +82,6 @@ jobs:
       - uses: Swatinem/rust-cache@v2
         with:
           workspaces: lib
-
-      - name: Build Kotlin binding
-        if: ${{ inputs.kotlin }}
-        working-directory: lib/bindings
-        run: |
-          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language kotlin -o ffi/kotlin
-  
-      - name: Archive Kotlin binding
-        if: ${{ inputs.kotlin }}
-        uses: actions/upload-artifact@v4
-        with:
-          name: bindings-kotlin
-          path: lib/bindings/ffi/kotlin/breez_sdk_liquid/breez_sdk_liquid.kt
       
       - name: Build Swift binding
         if: ${{ inputs.swift }}
@@ -154,6 +141,13 @@ jobs:
         working-directory: lib/bindings
         run: |
           cargo run --no-default-features --features=uniffi-25 --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --language kotlin -o ffi/kotlin
+  
+      - name: Archive Kotlin binding
+        if: ${{ inputs.kotlin }}
+        uses: actions/upload-artifact@v4
+        with:
+          name: bindings-kotlin
+          path: lib/bindings/ffi/kotlin/breez_sdk_liquid/breez_sdk_liquid.kt
 
       - name: Archive Kotlin multiplatform binding
         if: ${{ inputs.kotlin }}

.github/workflows/main.yml
@@ -217,8 +217,8 @@ jobs:
 
       - name: Run bindings tests
         run: |
-          curl -o jna-5.12.1.jar https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar
-          export CLASSPATH=$(pwd)/jna-5.12.1.jar;
+          curl -o jna-5.14.0.jar https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.14.0/jna-5.14.0.jar
+          export CLASSPATH=$(pwd)/jna-5.14.0.jar;
           cd lib/bindings
           cargo test
 
@@ -335,7 +335,7 @@ jobs:
       - name: Build Android bindings
         working-directory: lib/bindings       
         run: |
-          cargo run --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o langs/android/lib/src/main/kotlin
+          cargo run --no-default-features --features=uniffi-25 --bin uniffi-bindgen generate src/breez_sdk_liquid.udl --no-format --language kotlin -o langs/android/lib/src/main/kotlin
 
       - name: Run Android build
         working-directory: lib/bindings/langs/android  

.github/workflows/publish-all-platforms.yml
@@ -121,7 +121,7 @@ jobs:
       swift-package-version: ${{ needs.pre-setup.outputs.swift-package-version || '0.0.2' }}
       publish: ${{ needs.pre-setup.outputs.publish }}
       use-dummy-binaries: ${{ needs.pre-setup.outputs.use-dummy-binaries }}
-      uniffi-25: ${{ !!needs.pre-setup.outputs.csharp-package-version || !!needs.pre-setup.outputs.golang-package-version || !!needs.pre-setup.outputs.kotlin-multiplatform-package-version }}
+      uniffi-25: ${{ !!needs.pre-setup.outputs.csharp-package-version || !!needs.pre-setup.outputs.golang-package-version || !!needs.pre-setup.outputs.kotlin-multiplatform-package-version || !!needs.pre-setup.outputs.maven-package-version }}
     steps:
       - run: echo "set setup output variables"
 

.github/workflows/publish-android.yml
@@ -44,7 +44,7 @@ jobs:
 
       - uses: actions/download-artifact@v4
         with:
-          name: bindings-android-jniLibs
+          name: bindings-android-jniLibs-uniffi-25
           path: lib/bindings/langs/android/lib/src/main/jniLibs
 
       - uses: actions/download-artifact@v4

lib/bindings/langs/android/lib/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 33
+        minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
     }
     

lib/bindings/langs/kotlin-multiplatform/breez-sdk-liquid-kmp/build.gradle.kts
@@ -83,7 +83,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 24
         consumerProguardFiles("consumer-rules.pro")
     }
 

lib/bindings/makefile
@@ -58,7 +58,7 @@ x86_64-linux-android-uniffi-25: $(SOURCES) ndk-home
 	cargo ndk -t x86_64-linux-android -o ffi/kotlin/jniLibs build --no-default-features --features=uniffi-25 --release
 	cp -a $(ANDROID_NDK_HOME)/toolchains/llvm/prebuilt/$(OS_NAME)-x86_64/sysroot/usr/lib/x86_64-linux-android/libc++_shared.so ffi/kotlin/jniLibs/x86_64/
 
-bindings-android: android
+bindings-android: android-uniffi-25
 	cp -r ffi/kotlin/jniLibs langs/android/lib/src/main
 	cp -r ffi/kotlin/breez_sdk_liquid langs/android/lib/src/main/kotlin/
 	cd langs/android && ./gradlew assemble

packages/flutter/android/build.gradle
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
     }
 }
 

packages/flutter/android/build.gradle.production
@@ -51,7 +51,7 @@ android {
     }
 
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
     }
 }
 

packages/react-native/android/build.gradle
@@ -26,7 +26,7 @@ apply plugin: 'kotlin-android'
 android {
     compileSdkVersion 34
     defaultConfig {
-        minSdkVersion 33
+        minSdkVersion 24
         targetSdkVersion 34
         versionCode 1
         versionName "1.0"

packages/react-native/example/android/build.gradle
@@ -3,7 +3,7 @@
 buildscript {
     ext {
         buildToolsVersion = "33.0.0"
-        minSdkVersion = 33
+        minSdkVersion = 24
         compileSdkVersion = 34
         targetSdkVersion = 34
         kotlin_version = "1.8.0"