524cbfb9ee388d6e578878a2dbc9c3eabf8dcebd
Ross Savage <mr.dangeross@googlemail.com>
2025-04-06 22:13:49 +0200

    Add the remaining CLI commands for the node.js example

--- packages/wasm/examples/node/src/cli.js
+++ packages/wasm/examples/node/src/cli.js
@@ -1,180 +1,286 @@
-const { connect, defaultConfig, setLogger } = require('@breeztech/breez-sdk-liquid/node')
-const { Command, Option } = require('commander')
-const fs = require('fs')
-const qrcode = require('qrcode')
-const { confirm, command } = require('./prompt.js');
-const readline = require('readline');
-const { parse } = require('shell-quote')
-require('dotenv').config()
-
-const logFile = fs.createWriteStream(__dirname + '/../sdk.log', {flags : 'a'})
-
-class JsFileLogger {
-    log = (logEntry) => {
-        const logMessage = `[${new Date().toISOString()} ${logEntry.level}]: ${logEntry.line}\n`
-        logFile.write(logMessage)
-    }
-}
-
-const fileLogger = new JsFileLogger()
-
-class JsEventListener {
-    onEvent = (event) => {
-        fileLogger.log({
-            level: 'INFO',
-            line: `Received event: ${JSON.stringify(event)}`
-        })
-    }
-}
-
-const eventListener = new JsEventListener()
-
-let sdk = null
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: true
-})
-
-const shutdown = () => {
-    if (sdk) {
-        sdk.disconnect();
-    }
-    rl.close()
-    process.exit(0)
-}
+const { Command, Option, Argument } = require('commander')
+const { prompt } = require('./prompt.js')
+const {
+    buyBitcoin,
+    checkMessage,
+    disconnect,
+    getInfo,
+    getPayment,
+    listFiat,
+    listPayments,
+    listRefundables,
+    lnurlAuth,
+    lnurlPay,
+    lnurlWithdraw,
+    fetchFiatRates,
+    fetchLightningLimits,
+    fetchOnchainLimits,
+    parse,
+    prepareRefund,
+    receivePayment,
+    recommendedFees,
+    refund,
+    registerWebhook,
+    rescanOnchainSwaps,
+    reviewPaymentProposedFees,
+    sendOnchainPayment,
+    sendPayment,
+    signMessage,
+    sync,
+    unregisterWebhook
+} = require('./action.js')
+const { parse: parseShell } = require('shell-quote')
 
 const initCommand = () => {
     const program = new Command()
     program.exitOverride()
     program.name('nodeless-wasm-cli').description('CLI for Breez SDK - Nodeless Wasm')
-    
-    program.command('exit').action(shutdown)
 
-    program.command('get-info').action(async () => {
-        const sdk = await initSdk()
-        const res = await sdk.getInfo()
-        console.log(JSON.stringify(res, null, 2))
-    })
-    
-    program.command('list-payments').action(async () => {
-        const sdk = await initSdk()
-        const res = await sdk.listPayments({})
-        console.log(JSON.stringify(res, null, 2))
-    })
-    
-    program.command('receive-payment')
-        .addOption(new Option('-m, --payment-method <choice>', 'The method to use when receiving')
-            .makeOptionMandatory(true)
-            .choices(['lightning', 'bitcoinAddress', 'liquidAddress']))
-        .addOption(new Option('--amount-sat <number>', 'The amount the payer should send, in satoshi. If not specified, it will generate a BIP21 URI/address with no amount')
-            .argParser(parseInt))
-        .addOption(new Option('--asset <string>', 'Optional id of the asset to receive when the payment method is "liquidAddress"'))
-        .addOption(new Option('--amount <number>', 'The amount the payer should send, in asset units. If not specified, it will generate a BIP21 URI/address with no amount. The asset id must also be provided').argParser(parseFloat))
-        .action(async (options) => {
-            const sdk = await initSdk()
-            const prepareResponse = await sdk.prepareReceivePayment({
-                paymentMethod: options.paymentMethod,
-                amount: options.asset ? 
-                    { type: 'asset', assetId: options.asset, payerAmount: options.amount } : 
-                    { type: 'bitcoin', payerAmountSat: options.amountSat }
-            })
-            const fees = prepareResponse.feesSat
-            const message = options.amount ? 
-                `Fees: ${fees} sat + ${prepareResponse.swapperFeerate}% of the sent amount. Sender should send between ${prepareResponse.minPayerAmountSat} sat and ${prepareResponse.maxPayerAmountSat} sat. Are the fees acceptable?` :
-                `Fees: ${fees} sat. Are the fees acceptable?`
-            if (await confirm(rl, message)) {
-                const res = await sdk.receivePayment({ prepareResponse })
-                console.log(JSON.stringify(res, null, 2))
-                qrcode.toString(res.destination, { type: 'terminal', small: true }, (err, url) => {
-                    console.log(url)
-                })
-            }
-        })
+    program
+        .command('buy-bitcoin')
+        .description('Generates an URL to buy bitcoin from a 3rd party provider')
+        .addArgument(new Argument('<provider>', 'The provider to use').choices(['moonpay']))
+        .addArgument(new Argument('<amount-sat>', 'Amount to buy, in satoshi'))
+        .action(buyBitcoin)
 
-    program.command('send-payment')
+    program
+        .command('check-message')
+        .description('Verify a message with a public key')
+        .addArgument(new Argument('<message>', 'The message signed'))
+        .addArgument(new Argument('<pubkey>', 'The pubkey to verify against'))
+        .addArgument(new Argument('<signature>', 'The signature to verify'))
+        .action(checkMessage)
+
+    program.command('disconnect').alias('exit').description('Exit the CLI').action(disconnect)
+
+    program.command('get-info').description('Get the balance and general info of the current instance').action(getInfo)
+
+    program
+        .command('get-payment')
+        .description('Retrieve a payment')
+        .addOption(new Option('-p, --payment-hash <string>', 'Lightning payment hash'))
+        .addOption(new Option('-s, --swap-id <string>', 'Swap ID or its hash'))
+        .action(getPayment)
+
+    program.command('fetch-fiat-rates').description('Fetch available fiat rates').action(fetchFiatRates)
+
+    program
+        .command('fetch-lightning-limits')
+        .description('Fetch the current limits for Send and Receive payments')
+        .action(fetchLightningLimits)
+
+    program
+        .command('fetch-onchain-limits')
+        .description('Fetch the current limits for Onchain Send and Receive payments')
+        .action(fetchOnchainLimits)
+
+    program.command('list-fiat').description('List fiat currencies').action(listFiat)
+
+    program
+        .command('list-payments')
+        .description('List incoming and outgoing payments')
+        .addOption(new Option('-r, --filter <choice>', 'The optional payment type filter').choices(['send', 'receive']))
+        .addOption(
+            new Option('-s, --state <choice>', 'The optional payment state').choices([
+                'pending',
+                'complete',
+                'failed',
+                'pendingRefund',
+                'refundable'
+            ])
+        )
+        .addOption(new Option('-f, --from <number>', 'The optional from unix timestamp').argParser(parseInt))
+        .addOption(new Option('-t, --to <number>', 'The optional to unix timestamp').argParser(parseInt))
+        .addOption(new Option('-l, --limit <number>', 'Optional limit of listed payments').argParser(parseInt))
+        .addOption(new Option('-o, --offset <number>', 'Optional offset of listed payments').argParser(parseInt))
+        .addOption(new Option('--asset <string>', 'Optional id of the asset for Liquid payment method'))
+        .addOption(
+            new Option('-d, --destination <string>', 'Optional Liquid BIP21 URI/address for Liquid payment method')
+        )
+        .addOption(new Option('-a, --address <string>', 'Optional Liquid/Bitcoin address for Bitcoin payment method'))
+        .addOption(new Option('--ascending', 'Whether or not to sort the payments by ascending timestamp'))
+        .action(listPayments)
+
+    program.command('list-refundables').description('List refundable chain swaps').action(listRefundables)
+
+    program
+        .command('lnurl-auth')
+        .description('Authorize using LNURL')
+        .addArgument(new Argument('<lnurl>', 'LNURL-auth encoded endpoint'))
+        .action(lnurlAuth)
+
+    program
+        .command('lnurl-pay')
+        .description('Pay using LNURL')
+        .addArgument(new Argument('<lnurl>', 'LNURL-pay encoded endpoint'))
+        .addOption(
+            new Option(
+                '-d, --drain',
+                'Whether or not this is a drain operation. If true, all available funds will be used'
+            )
+        )
+        .addOption(new Option('-v, --validate', 'Validates the success action URL'))
+        .action(lnurlPay)
+
+    program
+        .command('lnurl-withdraw')
+        .description('Withdraw using LNURL')
+        .addArgument(new Argument('<lnurl>', 'LNURL-withdraw encoded endpoint'))
+        .action(lnurlWithdraw)
+
+    program
+        .command('parse')
+        .description('Parse a generic string to get its type and relevant metadata')
+        .addArgument(new Argument('<input>', 'Generic input (URL, LNURL, BIP-21 Bitcoin Address, LN invoice, etc)'))
+        .action(parse)
+
+    program
+        .command('prepare-refund')
+        .description('Prepare a refund transaction for an incomplete swap')
+        .addArgument(new Argument('<swap-address>', 'Swap address of the lockup'))
+        .addArgument(new Argument('<refund-address>', 'Bitcoin onchain address to send the refund to'))
+        .addArgument(new Argument('<fee-rate>', 'Fee rate to use, in sat/vbyte'))
+        .action(prepareRefund)
+
+    program
+        .command('receive-payment')
+        .description('Receive a payment directly or via a swap')
+        .addOption(
+            new Option('-m, --payment-method <choice>', 'The method to use when receiving')
+                .makeOptionMandatory(true)
+                .choices(['lightning', 'bitcoinAddress', 'liquidAddress'])
+        )
+        .addOption(
+            new Option(
+                '--amount-sat <number>',
+                'The amount the payer should send, in satoshi. If not specified, it will generate a BIP21 URI/address with no amount'
+            ).argParser(parseInt)
+        )
+        .addOption(
+            new Option(
+                '--asset <string>',
+                'Optional id of the asset to receive when the payment method is "liquidAddress"'
+            )
+        )
+        .addOption(
+            new Option(
+                '--amount <number>',
+                'The amount the payer should send, in asset units. If not specified, it will generate a BIP21 URI/address with no amount. The asset id must also be provided'
+            ).argParser(parseFloat)
+        )
+        .action(receivePayment)
+
+    program.command('recommended-fees').description('Get the recommended Bitcoin fees').action(recommendedFees)
+
+    program
+        .command('refund')
+        .description('Broadcast a refund transaction for an incomplete swap')
+        .addArgument(new Argument('<swap-address>', 'Swap address of the lockup'))
+        .addArgument(new Argument('<refund-address>', 'Bitcoin onchain address to send the refund to'))
+        .addArgument(new Argument('<fee-rate>', 'Fee rate to use, in sat/vbyte'))
+        .action(refund)
+
+    program
+        .command('register-webhook')
+        .description('Register a webhook URL')
+        .addArgument(new Argument('<url>', 'The URL to register'))
+        .action(registerWebhook)
+
+    program.command('rescan-onchain-swaps').description('Rescan onchain swaps').action(rescanOnchainSwaps)
+
+    program
+        .command('review-payment-proposed-fees')
+        .description('Get and potentially accept proposed fees for WaitingFeeAcceptance Payment')
+        .addArgument(new Argument('<swap-id>', 'The swap id of the payment to review'))
+        .action(reviewPaymentProposedFees)
+
+    program
+        .command('send-onchain-payment')
+        .description('Send to a Bitcoin onchain address via a swap')
+        .addArgument(new Argument('<address>', 'Bitcoin onchain address to send to'))
+        .addOption(
+            new Option(
+                '--receiver-amount-sat <number>',
+                'Amount that will be received, in satoshi. Must be set if not draining'
+            ).argParser(parseInt)
+        )
+        .addOption(
+            new Option(
+                '-d, --drain',
+                'Whether or not this is a drain operation. If true, all available funds will be used'
+            )
+        )
+        .addOption(
+            new Option('-f, --fee-rate <number>', 'The optional fee rate to use, in sat/vbyte').argParser(parseInt)
+        )
+        .action(sendOnchainPayment)
+
+    program
+        .command('send-payment')
+        .description('Send a payment directly or via a swap')
         .addOption(new Option('-i, --invoice <string>', 'Invoice which has to be paid'))
         .addOption(new Option('-o, --offer <string>', 'BOLT12 offer. If specified, amount-sat must also be set'))
         .addOption(new Option('-a, --address <string>', 'Either BIP21 URI or Liquid address we intend to pay to'))
-        .addOption(new Option('--amount-sat <number>', 'The amount the payer should send, in satoshi. If not specified, it will generate a BIP21 URI/address with no amount')
-            .argParser(parseInt))
-        .addOption(new Option('--asset <string>', 'Optional id of the asset to receive when the payment method is "liquidAddress"'))
-        .addOption(new Option('--amount <number>', 'The amount the payer should send, in asset units. If not specified, it will generate a BIP21 URI/address with no amount. The asset id must also be provided').argParser(parseFloat))
-        .addOption(new Option('-d --drain', 'Whether or not this is a drain operation. If true, all available funds will be used'))
-        .action(async (options) => {
-            const sdk = await initSdk()
-            const destination = options.invoice || options.offer || options.address
-            const amount = options.drain ? { type: 'drain' } : 
-                options.asset ? { type: 'asset', assetId: options.asset, receiverAmount: options.amount } : 
-                options.amountSat ? { type: 'bitcoin', receiverAmountSat: options.amountSat } : undefined
-            if (!destination) {
-                console.error('Please provide either an invoice, offer or address')
-                return
-            }
-            if (options.offer && !options.amountSat) {
-                console.error('Please provide an amount in satoshi')
-                return
-            }
-            if (amount && amount.type === 'asset' && !amount.receiverAmount) {
-                console.error('Please provide an amount in asset units')
-                return
-            }
-            const prepareResponse = await sdk.prepareSendPayment({
-                destination,
-                amount
-            })
-            const message = `Fees: ${prepareResponse.feesSat} sat. Are the fees acceptable?`
-            if (await confirm(rl, message)) {
-                const res = await sdk.sendPayment({ prepareResponse })
-                console.log(JSON.stringify(res, null, 2))
-            }
-        })
+        .addOption(
+            new Option(
+                '--amount-sat <number>',
+                'The amount the payer should send, in satoshi. If not specified, it will generate a BIP21 URI/address with no amount'
+            ).argParser(parseInt)
+        )
+        .addOption(
+            new Option(
+                '--asset <string>',
+                'Optional id of the asset to receive when the payment method is "liquidAddress"'
+            )
+        )
+        .addOption(
+            new Option(
+                '--amount <number>',
+                'The amount the payer should send, in asset units. If not specified, it will generate a BIP21 URI/address with no amount. The asset id must also be provided'
+            ).argParser(parseFloat)
+        )
+        .addOption(
+            new Option(
+                '-d, --drain',
+                'Whether or not this is a drain operation. If true, all available funds will be used'
+            )
+        )
+        .action(sendPayment)
+
+    program
+        .command('sign-message')
+        .description('Sign a message using the wallet private key')
+        .addArgument(new Argument('<message>', 'The message to sign'))
+        .action(signMessage)
+
+    program.command('sync').description('Sync local data with mempool and onchain data').action(sync)
+
+    program.command('unregister-webhook').description('Unregister the webhook URL').action(unregisterWebhook)
 
     return program
-}
-
-const initSdk = async () => {
-    if (sdk) return sdk
-
-    // Set the logger to trace
-    setLogger(fileLogger)
-
-    // Get the mnemonic
-    const breezApiKey = process.env.BREEZ_API_KEY
-    const mnemonic = process.env.MNEMONIC
-
-    // Connect using the config
-    let config = defaultConfig('mainnet', breezApiKey)
-    config.workingDir = "./.data"
-
-    sdk = await connect({ config, mnemonic })
-
-    await sdk.addEventListener(eventListener)
-    return sdk
 }
 
 const main = () => {
     return new Promise(async (resolve) => {
         while (true) {
             try {
-                const res = await command(rl, 'sdk')
+                const res = await prompt('sdk')
                 if (res.trim().toLowerCase() === 'exit') {
-                    shutdown()
+                    disconnect()
                     resolve()
                     break
                 } else {
                     const cmd = res.length > 0 ? res : '-h'
                     const program = initCommand()
-                    await program.parseAsync(parse(cmd), { from: 'user' })   
+                    await program.parseAsync(parseShell(cmd), { from: 'user' })
                 }
             } catch (e) {
-                if (e.code !== 'commander.helpDisplayed') {
+                if (!e.code) {
                     console.error('Error:', e)
                 }
             }
-        } 
-    })   
+        }
+    })
 }
 
 main()
