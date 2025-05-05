f5f02f2e4a926677d3ee91dd7b58dfe66b27a4fb
yse <hydra_yse@proton.me>
2025-05-05 12:14:37 +0200

    fix[side-swap]: switch to new api
    feat[side-swap]: update service name and add start/stop methods

--- lib/core/src/model.rs
+++ lib/core/src/model.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context as _, Result};
 use bitcoin::{bip32, ScriptBuf};
 use boltz_client::{
     boltz::{ChainPair, BOLTZ_MAINNET_URL_V2, BOLTZ_REGTEST, BOLTZ_TESTNET_URL_V2},
@@ -2406,6 +2406,51 @@ impl From<&lwk_wollet::History> for LBtcHistory {
 }
 pub(crate) type BtcScript = bitcoin::ScriptBuf;
 pub(crate) type LBtcScript = elements::Script;
+
+/// The current state of a swap via the SideSwap service
+#[derive(Debug, Clone, Serialize)]
+pub struct AssetSwap {
+    /// The identifier of the asset we are currently trading for
+    pub asset_id: String,
+    /// The exchange rate of the asset, or the total price for one L-BTC
+    pub exchange_rate: f64,
+    /// The asset amount which will be received after swapping
+    pub receiver_amount: u64,
+    /// The service fees for the swap
+    pub fees_sat: u64,
+    /// The amount of L-BTC (in satoshis) to execute the swap
+    pub payer_amount_sat: u64,
+}
+
+impl TryFrom<&sideswap_api::SubscribePriceStreamResponse> for AssetSwap {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &sideswap_api::SubscribePriceStreamResponse) -> Result<Self, Self::Error> {
+        if let Some(err) = &value.error_msg {
+            anyhow::bail!(
+                "Could not convert SideSwap price - received error message from stream: {err}"
+            );
+        }
+
+        Ok(Self {
+            asset_id: value.asset.to_string(),
+            payer_amount_sat: value
+                .send_amount
+                .context("Expected send amount when creating side swap")?
+                as u64,
+            receiver_amount: value
+                .recv_amount
+                .context("Expected receive amount when creating side swap")?
+                as u64,
+            fees_sat: value
+                .fixed_fee
+                .context("Expected fees when creating side swap")? as u64,
+            exchange_rate: value
+                .price
+                .context("Expected price when creating side swap")?,
+        })
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct BtcScriptBalance {
--- lib/core/src/side_swap/api/mod.rs
+++ lib/core/src/side_swap/api/mod.rs
@@ -1,17 +1,21 @@
-use anyhow::{anyhow, Context as _, Result};
+use anyhow::{anyhow, bail, Result};
 use base64::Engine;
 use futures_util::{SinkExt as _, StreamExt};
-use log::{error, info, warn};
-use notifications::SideSwapNotificationsHandler;
+use log::{debug, error, info, warn};
+use maybe_sync::{MaybeSend, MaybeSync};
 use request_handler::SideSwapRequestHandler;
 use response_handler::SideSwapResponseHandler;
+use sdk_common::prelude::RestClient;
 use sdk_common::utils::Arc;
-use sideswap_api::mkt::{
-    AssetPair, AssetType, GetQuoteRequest, QuoteNotif, QuoteStatus, QuoteSubId, StartQuotesRequest,
-    StartQuotesResponse, StopQuotesRequest, TakerSignRequest, TradeDir,
+use sideswap_api::http_rpc::{
+    SwapSignRequest, SwapSignResponse, SwapStartRequest, SwapStartResponse,
 };
-use sideswap_api::{mkt::Request as MarketRequest, mkt::Response as MarketResponse, Request};
-use sideswap_api::{RequestId, RequestMessage, Response, ResponseMessage};
+use sideswap_api::{
+    Notification, Request, RequestId, RequestMessage, Response, ResponseMessage,
+    StartSwapWebRequest, StartSwapWebResponse, SubscribePriceStreamRequest,
+    SubscribePriceStreamResponse, UnsubscribePriceStreamRequest,
+};
+use std::collections::HashMap;
 use std::str::FromStr;
 use std::time::Duration;
 use tokio::sync::mpsc::{Receiver, Sender};
@@ -22,30 +26,29 @@ use boltz_client::boltz::tokio_tungstenite_wasm;
 use tokio_tungstenite_wasm::{connect, Message};
 
 use crate::bitcoin::base64;
-use crate::elements::{self, pset::PartiallySignedTransaction, Txid};
+use crate::elements::{self, pset::PartiallySignedTransaction, AssetId, Txid};
+use crate::model::AssetSwap;
 use crate::wallet::OnchainWallet;
 
-mod notifications;
 mod request_handler;
 mod response_handler;
 
 pub const SIDESWAP_MAINNET_URL: &str = "wss://api.sideswap.io/json-rpc-ws";
 pub const SIDESWAP_TESTNET_URL: &str = "wss://api-testnet.sideswap.io/json-rpc-ws";
+pub const SIDESWAP_REGTEST_URL: &str = "wss://api-regtest.sideswap.io/json-rpc-ws";
 
 #[sdk_macros::async_trait]
-pub trait SideSwapStream {
-    async fn start_fetching_quotes(
+pub trait SideSwapService: MaybeSend + MaybeSync {
+    async fn start(self: Arc<Self>) -> Result<()>;
+    async fn stop(&self);
+    async fn subscribe_price_stream(
         &self,
-        asset_pair: AssetPair,
-        amount: u64,
-    ) -> Result<StartQuotesResponse>;
-    async fn stop_fetching_quotes(&self) -> Result<()>;
-    async fn get_quote(
-        &self,
-        quote_sub_id: QuoteSubId,
-        successful_only: bool,
-    ) -> Result<QuoteNotif>;
-    async fn sign_quote_pset(&self, quote_sub_id: QuoteSubId) -> Result<Txid>;
+        asset_id: AssetId,
+        send_amount_sat: u64,
+    ) -> Result<AssetSwap>;
+    async fn unsubscribe_price_stream(&self) -> Result<()>;
+    async fn prepare_swap(&self) -> Result<StartSwapWebResponse>;
+    async fn execute_swap(&self, start_res: StartSwapWebResponse) -> Result<Txid>;
 }
 
 struct ShutdownChannel {
@@ -62,25 +65,31 @@ impl ShutdownChannel {
     }
 }
 
-pub(crate) struct SideSwapService {
+pub(crate) struct HybridSideSwapService {
     url: String,
     is_started: Mutex<bool>,
     shutdown: ShutdownChannel,
+    rest_client: Arc<dyn RestClient>,
     onchain_wallet: Arc<dyn OnchainWallet>,
     request_handler: SideSwapRequestHandler,
     response_handler: SideSwapResponseHandler,
-    notifications_handler: SideSwapNotificationsHandler,
+    ongoing_swap: Mutex<Option<AssetSwap>>,
 }
 
-impl SideSwapService {
-    pub(crate) fn new(url: String, onchain_wallet: Arc<dyn OnchainWallet>) -> Self {
+impl HybridSideSwapService {
+    pub(crate) fn new(
+        url: String,
+        rest_client: Arc<dyn RestClient>,
+        onchain_wallet: Arc<dyn OnchainWallet>,
+    ) -> Self {
         Self {
             url,
             is_started: Mutex::new(false),
             shutdown: ShutdownChannel::new(tokio::sync::mpsc::channel::<()>(10)),
             request_handler: SideSwapRequestHandler::new(),
             response_handler: SideSwapResponseHandler::new(),
-            notifications_handler: SideSwapNotificationsHandler::new(),
+            ongoing_swap: Mutex::new(None),
+            rest_client,
             onchain_wallet,
         }
     }
@@ -90,7 +99,54 @@ impl SideSwapService {
         *lock = is_started;
     }
 
+    async fn update_ongoing_swap(&self, res: &SubscribePriceStreamResponse) -> Result<AssetSwap> {
+        let asset_swap: AssetSwap = res.try_into()?;
+        let mut lock = self.ongoing_swap.lock().await;
+        *lock = Some(asset_swap.clone());
+        Ok(asset_swap)
+    }
+
+    async fn unset_ongoing_swap(&self) {
+        let mut lock = self.ongoing_swap.lock().await;
+        *lock = None;
+    }
+
+    async fn handle_message(&self, msg: &str) {
+        info!("Received text message: {msg:?}");
+        match serde_json::from_str::<ResponseMessage>(msg) {
+            Ok(ResponseMessage::Response(req_id, res)) => {
+                self.response_handler
+                    .handle_response(req_id.clone(), res)
+                    .await;
+            }
+            Ok(ResponseMessage::Notification(notif)) => match notif {
+                Notification::UpdatePriceStream(res) => {
+                    if let Err(err) = self.update_ongoing_swap(&res).await {
+                        warn!("Could not update ongoing swap: {err:?}");
+                    }
+                }
+                notif => debug!("Received unhandled notification from SideSwap service: {notif:?}"),
+            },
+            // Either an invalid response, or an error
+            Err(e) => error!("Failed to parse websocket response: {e:?} - response: {msg}"),
+        }
+    }
+
+    fn invalid_response(res: Result<Response, sideswap_api::Error>) -> anyhow::Error {
+        match res {
+            Ok(res) => anyhow!("Received invalid response from the server: {res:?}"),
+            Err(err) => anyhow!("Received error response from the server: {err:?}"),
+        }
+    }
+}
+
+#[sdk_macros::async_trait]
+impl SideSwapService for HybridSideSwapService {
     async fn start(self: Arc<Self>) -> Result<()> {
+        if *self.is_started.lock().await {
+            return Ok(());
+        }
+
         let ws = connect(&self.url).await?;
         self.set_started(true).await;
 
@@ -150,117 +206,127 @@ impl SideSwapService {
         Ok(())
     }
 
-    pub(crate) async fn stop(&self) {
+    async fn stop(&self) {
         let _ = self.shutdown.sender.send(()).await;
+        self.unset_ongoing_swap().await;
     }
 
-    async fn handle_message(&self, msg: &str) {
-        info!("Received text message: {msg:?}");
-        match serde_json::from_str::<ResponseMessage>(msg) {
-            Ok(ResponseMessage::Response(req_id, res)) => {
-                self.response_handler
-                    .handle_response(req_id.clone(), res)
-                    .await;
-            }
-            Ok(ResponseMessage::Notification(notif)) => {
-                self.notifications_handler.handle_notification(notif).await;
-            }
-            // Either an invalid response, or an error
-            Err(e) => error!("Failed to parse websocket response: {e:?} - response: {msg}"),
-        }
-    }
-
-    fn invalid_response(res: Result<Response, sideswap_api::Error>) -> anyhow::Error {
-        match res {
-            Ok(res) => anyhow!("Received invalid response from the server: {res:?}"),
-            Err(err) => anyhow!("Received error response from the server: {err:?}"),
-        }
-    }
-}
-
-#[sdk_macros::async_trait]
-impl SideSwapStream for SideSwapService {
-    async fn start_fetching_quotes(
+    async fn subscribe_price_stream(
         &self,
-        asset_pair: AssetPair,
-        amount: u64,
-    ) -> Result<StartQuotesResponse> {
-        let receive_address = self.onchain_wallet.next_unused_address().await?;
-        let change_address = self.onchain_wallet.next_unused_change_address().await?;
+        asset_id: AssetId,
+        send_amount_sat: u64,
+    ) -> Result<AssetSwap> {
+        let req = Request::SubscribePriceStream(SubscribePriceStreamRequest {
+            subscribe_id: None,
+            asset: asset_id,
+            send_bitcoins: true,
+            send_amount: Some(send_amount_sat as i64),
+            recv_amount: None,
+        });
+        let request_id = self.request_handler.send(req).await?;
+        let res = match self.response_handler.recv(request_id).await? {
+            Ok(Response::SubscribePriceStream(res)) => res,
+            res => {
+                return Err(Self::invalid_response(res));
+            }
+        };
 
-        let req = Request::Market(MarketRequest::StartQuotes(StartQuotesRequest {
-            asset_pair,
-            asset_type: AssetType::Base,
-            amount,
-            trade_dir: TradeDir::Buy,
-            utxos: vec![],
-            receive_address,
-            change_address,
-            order_id: None,
-            private_id: None,
-            instant_swap: false,
-        }));
+        self.update_ongoing_swap(&res).await
+    }
 
+    async fn unsubscribe_price_stream(&self) -> Result<()> {
+        let req =
+            Request::UnsubscribePriceStream(UnsubscribePriceStreamRequest { subscribe_id: None });
         let request_id = self.request_handler.send(req).await?;
         match self.response_handler.recv(request_id).await? {
-            Ok(Response::Market(MarketResponse::StartQuotes(res))) => Ok(res),
+            Ok(Response::UnsubscribePriceStream(_)) => {}
+            res => {
+                return Err(Self::invalid_response(res));
+            }
+        };
+        self.unset_ongoing_swap().await;
+        Ok(())
+    }
+
+    async fn prepare_swap(&self) -> Result<StartSwapWebResponse> {
+        let Some(ref ongoing_swap) = *self.ongoing_swap.lock().await else {
+            bail!("Cannot start swap without subscribing to price stream first");
+        };
+
+        let req = Request::StartSwapWeb(StartSwapWebRequest {
+            asset: AssetId::from_str(&ongoing_swap.asset_id)?,
+            price: ongoing_swap.exchange_rate,
+            send_amount: ongoing_swap.payer_amount_sat as i64,
+            recv_amount: ongoing_swap.receiver_amount as i64,
+            send_bitcoins: true,
+        });
+        let request_id = self.request_handler.send(req).await?;
+        match self.response_handler.recv(request_id).await? {
+            Ok(Response::StartSwapWeb(res)) => Ok(res),
             res => Err(Self::invalid_response(res)),
         }
     }
 
-    async fn stop_fetching_quotes(&self) -> Result<()> {
-        let req = Request::Market(MarketRequest::StopQuotes(StopQuotesRequest {}));
-        let request_id = self.request_handler.send(req).await?;
-        match self.response_handler.recv(request_id).await? {
-            Ok(Response::Market(MarketResponse::StopQuotes(_))) => Ok(()),
-            res => Err(Self::invalid_response(res)),
-        }
-    }
+    async fn execute_swap(&self, start_res: StartSwapWebResponse) -> Result<Txid> {
+        let recv_addr = self.onchain_wallet.next_unused_address().await?;
+        let change_addr = self.onchain_wallet.next_unused_change_address().await?;
 
-    async fn get_quote(
-        &self,
-        quote_sub_id: QuoteSubId,
-        successful_only: bool,
-    ) -> Result<QuoteNotif> {
-        let maybe_quote = self
-            .notifications_handler
-            .wait_for_quote(
-                quote_sub_id,
-                Duration::from_millis(500),
-                10,
-                successful_only,
+        let body = serde_json::to_string(&SwapStartRequest {
+            order_id: start_res.order_id,
+            inputs: vec![],
+            recv_addr,
+            change_addr,
+            send_asset: start_res.send_asset,
+            send_amount: start_res.send_amount,
+            recv_asset: start_res.recv_asset,
+            recv_amount: start_res.recv_amount,
+        })?;
+        let (raw_body, status) = self
+            .rest_client
+            .post(
+                &start_res.upload_url,
+                Some(HashMap::from([(
+                    "Content-Type".to_string(),
+                    "application/json".to_string(),
+                )])),
+                Some(body),
             )
-            .await;
-        maybe_quote.context("Did not receive any quotes from server")
-    }
+            .await?;
 
-    async fn sign_quote_pset(&self, quote_sub_id: QuoteSubId) -> Result<Txid> {
-        let quote = self.get_quote(quote_sub_id, true).await?;
-        let quote_id = match quote.status {
-            QuoteStatus::Success { quote_id, .. } => quote_id,
-            _ => anyhow::bail!("Expected quote with success status, got: {quote:?}"),
-        };
+        if !reqwest::StatusCode::from_u16(status)?.is_success() {
+            bail!("Received error status code when executing swap: {status}");
+        }
 
-        let req = Request::Market(MarketRequest::GetQuote(GetQuoteRequest { quote_id }));
-        let request_id = self.request_handler.send(req).await?;
-        let get_quote_res = match self.response_handler.recv(request_id).await? {
-            Ok(Response::Market(MarketResponse::GetQuote(res))) => res,
-            res => return Err(Self::invalid_response(res)),
-        };
-
-        let pset = PartiallySignedTransaction::from_str(&get_quote_res.pset)?;
+        let response: SwapStartResponse = serde_json::from_str(&raw_body)?;
+        let pset = PartiallySignedTransaction::from_str(&response.pset)?;
         // TODO verify pset amounts match
 
         let tx = self.onchain_wallet.sign_pset(pset).await?;
         let tx = elements::encode::serialize(&tx);
-        let req = Request::Market(MarketRequest::TakerSign(TakerSignRequest {
-            quote_id,
+        let body = serde_json::to_string(&SwapSignRequest {
+            order_id: start_res.order_id,
+            submit_id: response.submit_id,
             pset: base64::engine::general_purpose::STANDARD.encode(&tx),
-        }));
-        let request_id = self.request_handler.send(req).await?;
-        match self.response_handler.recv(request_id).await? {
-            Ok(Response::Market(MarketResponse::TakerSign(res))) => Ok(res.txid),
-            res => Err(Self::invalid_response(res)),
+        })?;
+
+        let (raw_body, status) = self
+            .rest_client
+            .post(
+                &start_res.upload_url,
+                Some(HashMap::from([(
+                    "Content-Type".to_string(),
+                    "application/json".to_string(),
+                )])),
+                Some(body),
+            )
+            .await?;
+
+        if !reqwest::StatusCode::from_u16(status)?.is_success() {
+            bail!("Received error status code when sending pset: {status}");
         }
+
+        let response: SwapSignResponse = serde_json::from_str(&raw_body)?;
+        self.unset_ongoing_swap().await;
+        Ok(response.txid)
     }
 }
