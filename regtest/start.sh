778b6f5e10e2cd19d90f452ec97820001587cc04
Daniel Granhão <daniel.granhao@gmail.com>
2025-06-05 17:25:34 +0100

    Add swapproxy to regtest environment

--- regtest/start.sh
+++ regtest/start.sh
@@ -4,8 +4,12 @@ set -xe
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "$SCRIPT_DIR/boltz"
+# Use boltz from https://github.com/BoltzExchange/boltz-backend/pull/959 while it isn't merged
+export BOLTZ_BACKEND_IMAGE=danielgranhao/boltz:latest
 ./start.sh
 
 cd "$SCRIPT_DIR"
 docker compose down
 docker compose up --remove-orphans -d
+
+./swapproxy-db-tool.sh --migrate
